4c53a6cca8b885e594b461088b1ab08e709c1f06
KsArT-IT <ksart.it@gmail.com>
2025-03-10 20:06:12 +0200

    feat: initialize service locator in main application entry point

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:habit_current/app/habit_current_app.dart';
+import 'package:habit_current/core/di/service_locator.dart';
 
 void main() {
+  setupServiceLocator();    
   runApp(const HabitCurrentApp());
 }
