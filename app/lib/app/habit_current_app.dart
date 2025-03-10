5978baf9e05a13665395983582ad37183965caa5
KsArT-IT <ksart.it@gmail.com>
2025-03-10 20:06:44 +0200

    feat: implement router configuration and localization support in HabitCurrentApp

--- app/lib/app/habit_current_app.dart
+++ app/lib/app/habit_current_app.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:habit_current/features/ui/habit_flow_screen.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:habit_current/core/di/service_locator.dart';
+import 'package:habit_current/core/router/app_router.dart';
+import 'package:habit_current/core/theme/app_theme.dart';
+import 'package:habit_current/generated/l10n.dart';
 
 class HabitCurrentApp extends StatelessWidget {
   const HabitCurrentApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Habit Current',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const HabitFlowScreen(title: 'Habit Flow'),
+    return MaterialApp.router(
+      onGenerateTitle: (context) => S.of(context).appTitle,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system, // TODO: get from settings
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      locale: const Locale('en'), // TODO: get from settings
+      routerConfig: getIt<AppRouter>().config(),
     );
   }
 }
