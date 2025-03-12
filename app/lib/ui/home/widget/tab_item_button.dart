f2ae7117855d063ddbea047a7494cc67f163078f
KsArT-IT <ksart.it@gmail.com>
2025-03-12 20:58:42 +0200

    refactor: update TabItemButton to use AppConstants for size and improve color handling

--- app/lib/ui/home/widget/tab_item_button.dart
+++ app/lib/ui/home/widget/tab_item_button.dart
@@ -1,46 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:habit_current/core/utils/constants.dart';
 import 'package:habit_current/ui/home/home_tab.dart';
 
 class TabItemButton extends StatelessWidget {
   final HomeTab tab;
   final int active;
   final Function(int) onTap;
-  final size = 70.0;
 
-  const TabItemButton({
-    super.key,
-    required this.tab,
-    required this.active,
-    required this.onTap,
-  });
+  const TabItemButton({super.key, required this.tab, required this.active, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => onTap(tab.index),
-      borderRadius: BorderRadius.circular(size),
-      child: SizedBox(
-        width: size,
-        height: size,
+    final isActive = active == tab.index;
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return SizedBox(
+      width: AppConstants.tabItemButtonSize,
+      height: AppConstants.tabItemButtonSize,
+      child: InkWell(
+        onTap: () => onTap(tab.index),
+        customBorder: const CircleBorder(),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(
-              tab.icon,
-              color:
-                  active == tab.index
-                      ? Theme.of(context).colorScheme.primary
-                      : Theme.of(context).colorScheme.surface,
-            ),
+            Icon(tab.icon, color: isActive ? colorScheme.primary : colorScheme.inversePrimary),
             Text(
               tab.getLabel(context),
               overflow: TextOverflow.ellipsis,
-              style: TextStyle(
-                color:
-                    active == tab.index
-                        ? Theme.of(context).colorScheme.primary
-                        : Theme.of(context).colorScheme.onSurface,
-              ),
+              style: TextStyle(color: isActive ? colorScheme.primary : colorScheme.inversePrimary, fontSize: 12),
             ),
           ],
         ),
