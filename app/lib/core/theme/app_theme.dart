ae749f4ae045e6bbc5fd1201643241317346fb3c
KsArT-IT <ksart.it@gmail.com>
2025-03-17 20:46:58 +0200

    feat: enhance color scheme for light and dark themes in AppTheme

--- app/lib/core/theme/app_theme.dart
+++ app/lib/core/theme/app_theme.dart
@@ -3,9 +3,27 @@ import 'package:flutter/material.dart';
 sealed class AppTheme {
   static final lightTheme = ThemeData.light(
     useMaterial3: true,
-  ).copyWith(appBarTheme: AppBarTheme(backgroundColor: Colors.indigo[300]));
+  ).copyWith(
+    appBarTheme: AppBarTheme(backgroundColor: Colors.indigo[300]),
+    colorScheme: ColorScheme.light().copyWith(
+      // Primary Button
+      secondary: Color.fromARGB(0xFF, 0x0F, 0x0F, 0x0F),
+      onSecondary: Color.fromARGB(0xFF, 0xF5, 0xF5, 0xF5),
+      // Onboard Background Brand Color
+      tertiary: Color.fromARGB(0xFF, 0xFA, 0xFC, 0x2C),//#FAFC2C
+      tertiaryContainer: Color.fromARGB(0xFF, 0xF5, 0xF5, 0xF5),
+    )
+  );
 
   static final darkTheme = ThemeData.dark(
     useMaterial3: true,
-  ).copyWith(appBarTheme: AppBarTheme(backgroundColor: Colors.indigo[900]));
+  ).copyWith(
+    appBarTheme: AppBarTheme(backgroundColor: Colors.indigo[900]),
+    colorScheme: ColorScheme.dark().copyWith(
+      secondary: Color.fromARGB(0xFF, 0xF5, 0xF5, 0xF5),
+      onSecondary: Color.fromARGB(0xFF, 0x0F, 0x0F, 0x0F),
+      tertiary: Color.fromARGB(0xFF, 0x0F, 0x0F, 0x0F),
+      tertiaryContainer: Color.fromARGB(0xFF, 0x0F, 0x0F, 0x0F),
+    )
+  );
 }
