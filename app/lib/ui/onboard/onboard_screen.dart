aac2c7086313ff452bbf5c78f2ccc2a7bfcc6e72
KsArT-IT <ksart.it@gmail.com>
2025-03-18 18:58:13 +0200

    feat: update OnboardScreen to use theme and router for improved navigation and styling

--- app/lib/ui/onboard/onboard_screen.dart
+++ app/lib/ui/onboard/onboard_screen.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:habit_current/core/constants/images.dart';
+import 'package:habit_current/core/router/app_router.gr.dart';
+import 'package:habit_current/core/theme/app_theme.dart';
 import 'package:habit_current/generated/l10n.dart';
 import 'package:habit_current/ui/widgets/primary_button.dart';
 
@@ -10,40 +11,44 @@ class OnboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        gradient: RadialGradient(
-          colors: [
-            Theme.of(context).colorScheme.tertiary,
-            Theme.of(context).colorScheme.tertiaryContainer,
-          ],
-          center: Alignment.topCenter,
-          stops: [0.3, 1.0],
-          radius: 2.3,
+    final theme = Theme.of(context);
+    final S strings = S.of(context);
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            colors: [
+              theme.colorScheme.tertiaryFixed,
+              theme.colorScheme.onTertiaryFixed,
+            ],
+            center: Alignment.topCenter,
+            stops: [0.3, 1.0],
+            radius: 2.3,
+          ),
         ),
-      ),
-      child: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: Image.asset(
-                ThemeImage.getPath('onboard_logo.png', context),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: Image.asset(theme.getImagePath('onboard_logo.png')),
               ),
-            ),
-            Expanded(
-              child: Column(
-                children: [
-                  Image.asset(ThemeImage.getPath('onboard_hello.png', context)),
-                  Spacer(),
-                  PrimaryButton(
-                    label: S.of(context).continues,
-                    onPressed: () {},
-                  ),
-                ],
+              Expanded(
+                child: Column(
+                  children: [
+                    Image.asset(theme.getImagePath('onboard_hello.png')),
+                    Spacer(),
+                    PrimaryButton(
+                      label: strings.continues,
+                      onPressed: () {
+                        context.router.replace(const HelloRoute());
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
