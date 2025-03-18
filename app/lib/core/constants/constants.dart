69e72664e26c99111a92f9c3dcd909aa422f3313
KsArT-IT <ksart.it@gmail.com>
2025-03-18 18:56:49 +0200

    feat: update Constants class to include text field dimensions and padding values

--- app/lib/core/constants/constants.dart
+++ app/lib/core/constants/constants.dart
@@ -1,4 +1,15 @@
-abstract class Constants {
-  static const double buttonHeight = 54;
+abstract final class Constants {
+  static const double buttonHeight = 48;
   static const double buttonRadius = buttonHeight / 2;
+
+  static const double textFieldHeight = 44;
+  static const double textFieldRadius = textFieldHeight / 2;
+  static const double textFieldPaddingHorizontal = 16;
+  static const double textFieldPaddingVertical = 12;
+  static const double textFieldSpacing = 24;
+
+  static const double paddingSmall = 8;
+  static const double paddingMedium = 16;
+  static const double paddingLarge = 24;
+  static const double paddingXLarge = 32;
 }
