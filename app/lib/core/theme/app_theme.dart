40b587ab19ceb21734f8e4b8157b402f8bbfb10c
KsArT-IT <ksart.it@gmail.com>
2025-03-18 18:57:17 +0200

    feat: enhance AppTheme with AppColors for improved color management

--- app/lib/core/theme/app_theme.dart
+++ app/lib/core/theme/app_theme.dart
@@ -1,29 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:habit_current/core/theme/colors.dart';
 
 sealed class AppTheme {
-  static final lightTheme = ThemeData.light(
-    useMaterial3: true,
-  ).copyWith(
-    appBarTheme: AppBarTheme(backgroundColor: Colors.indigo[300]),
+  static final lightTheme = ThemeData.light(useMaterial3: true).copyWith(
+    appBarTheme: AppBarTheme(backgroundColor: AppColors.gray50),
     colorScheme: ColorScheme.light().copyWith(
-      // Primary Button
-      secondary: Color.fromARGB(0xFF, 0x0F, 0x0F, 0x0F),
-      onSecondary: Color.fromARGB(0xFF, 0xF5, 0xF5, 0xF5),
-      // Onboard Background Brand Color
-      tertiary: Color.fromARGB(0xFF, 0xFA, 0xFC, 0x2C),//#FAFC2C
-      tertiaryContainer: Color.fromARGB(0xFF, 0xF5, 0xF5, 0xF5),
-    )
+      // Primary gray50 gray950
+      primary: AppColors.gray50,
+      onPrimary: AppColors.gray950,
+      
+      // Buttons
+      secondary: AppColors.gray950,
+      onSecondary: AppColors.gray50,
+      secondaryFixed: AppColors.gray300,
+      //
+      secondaryContainer: AppColors.gray950,
+      onSecondaryContainer: Color.fromARGB(0xFF, 0x0F, 0x0F, 0x0F),
+      tertiary: Color.fromARGB(0xFF, 0xF5, 0xF5, 0xF5),
+      onTertiary: Color.fromARGB(0xFF, 0x0F, 0x0F, 0x0F),
+      // Onboard Text Field
+      tertiaryContainer: AppColors.gray50,
+      onTertiaryContainer: AppColors.gray800,
+      tertiaryFixedDim: AppColors.gray300,
+      // Onboard Background
+      tertiaryFixed: AppColors.onboardColor,
+      onTertiaryFixed: AppColors.gray50,
+    ),
   );
 
-  static final darkTheme = ThemeData.dark(
-    useMaterial3: true,
-  ).copyWith(
-    appBarTheme: AppBarTheme(backgroundColor: Colors.indigo[900]),
+  static final darkTheme = ThemeData.dark(useMaterial3: true).copyWith(
+    appBarTheme: AppBarTheme(backgroundColor: AppColors.gray950),
     colorScheme: ColorScheme.dark().copyWith(
-      secondary: Color.fromARGB(0xFF, 0xF5, 0xF5, 0xF5),
-      onSecondary: Color.fromARGB(0xFF, 0x0F, 0x0F, 0x0F),
-      tertiary: Color.fromARGB(0xFF, 0x0F, 0x0F, 0x0F),
-      tertiaryContainer: Color.fromARGB(0xFF, 0x0F, 0x0F, 0x0F),
-    )
+      // Primary gray50 gray950
+      primary: AppColors.gray950,
+      onPrimary: AppColors.gray50,
+      // Buttons
+      secondary: AppColors.gray50,
+      onSecondary: AppColors.gray950,
+      secondaryFixed: AppColors.gray700,
+      //
+      secondaryContainer: Color.fromARGB(0xFF, 0x0F, 0x0F, 0x0F),
+      onSecondaryContainer: Color.fromARGB(0xFF, 0xF5, 0xF5, 0xF5),
+
+      tertiary: Color.fromARGB(0xFF, 0x1F, 0x1F, 0x1F),
+      onTertiary: Color.fromARGB(0xFF, 0xF5, 0xF5, 0xF5),
+      // Onboard Text Field
+      tertiaryContainer: AppColors.gray900,
+      onTertiaryContainer: AppColors.gray800,
+      tertiaryFixedDim: AppColors.gray900,
+      // Onboard Background
+      tertiaryFixed: AppColors.onboardColorDark,
+      onTertiaryFixed: AppColors.gray950,
+    ),
   );
+}
+
+extension ThemeX on ThemeData {
+  String get _themeFolder => brightness == Brightness.dark ? 'dark' : 'light';
+
+  String getImagePath(String imageName) => 'assets/images/$_themeFolder/$imageName';
 }
