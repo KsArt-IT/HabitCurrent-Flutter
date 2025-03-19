c99ff9dc99eb3a694f31de054b7e9e6a9628c9a1
KsArT-IT <ksart.it@gmail.com>
2025-03-19 17:40:35 +0200

    refactor: change onboard screen for use RadialGradientBackground

--- app/lib/ui/onboard/onboard_screen.dart
+++ app/lib/ui/onboard/onboard_screen.dart
@@ -4,6 +4,7 @@ import 'package:habit_current/core/router/app_router.gr.dart';
 import 'package:habit_current/core/theme/app_theme.dart';
 import 'package:habit_current/generated/l10n.dart';
 import 'package:habit_current/ui/widgets/primary_button.dart';
+import 'package:habit_current/ui/widgets/radial_gradient_background.dart';
 
 @RoutePage()
 class OnboardScreen extends StatelessWidget {
@@ -13,43 +14,25 @@ class OnboardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final S strings = S.of(context);
-    return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          gradient: RadialGradient(
-            colors: [
-              theme.colorScheme.tertiaryFixed,
-              theme.colorScheme.onTertiaryFixed,
-            ],
-            center: Alignment.topCenter,
-            stops: [0.3, 1.0],
-            radius: 2.3,
-          ),
-        ),
-        child: SafeArea(
-          child: Column(
-            children: [
-              Expanded(
-                child: Image.asset(theme.getImagePath('onboard_logo.png')),
-              ),
-              Expanded(
-                child: Column(
-                  children: [
-                    Image.asset(theme.getImagePath('onboard_hello.png')),
-                    Spacer(),
-                    PrimaryButton(
-                      label: strings.continues,
-                      onPressed: () {
-                        context.router.replace(const HelloRoute());
-                      },
-                    ),
-                  ],
+    return RadialGradientBackground(
+      body: Column(
+        children: [
+          Expanded(child: Image.asset(theme.getImagePath('onboard_logo.png'))),
+          Expanded(
+            child: Column(
+              children: [
+                Image.asset(theme.getImagePath('onboard_hello.png')),
+                Spacer(),
+                PrimaryButton(
+                  label: strings.continues,
+                  onPressed: () {
+                    context.router.replace(const HelloRoute());
+                  },
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
