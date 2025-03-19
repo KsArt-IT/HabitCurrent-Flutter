0290c1076f82b873452311da2c67f2ecb37bc6f4
KsArT-IT <ksart.it@gmail.com>
2025-03-19 17:40:53 +0200

    refactor: change hello screen for use RadialGradientBackground

--- app/lib/ui/hello/hello_screen.dart
+++ app/lib/ui/hello/hello_screen.dart
@@ -4,6 +4,7 @@ import 'package:habit_current/core/constants/constants.dart';
 import 'package:habit_current/core/router/app_router.gr.dart';
 import 'package:habit_current/generated/l10n.dart';
 import 'package:habit_current/ui/widgets/primary_button.dart';
+import 'package:habit_current/ui/widgets/radial_gradient_background.dart';
 
 @RoutePage()
 class HelloScreen extends StatefulWidget {
@@ -64,105 +65,91 @@ class _HelloScreenState extends State<HelloScreen> {
     final theme = Theme.of(context);
     final S strings = S.of(context);
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(Constants.paddingMedium),
-        decoration: BoxDecoration(
-          gradient: RadialGradient(
-            colors: [
-              theme.colorScheme.tertiaryFixed,
-              theme.colorScheme.onTertiaryFixed,
+      body: RadialGradientBackground(
+        body: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Text(strings.welcome, style: theme.textTheme.titleLarge),
+              Text(strings.enterYourName, style: theme.textTheme.bodyMedium),
+              Spacer(),
+              TextFormField(
+                controller: _nameController,
+                style: theme.textTheme.bodyMedium?.copyWith(
+                  color: theme.colorScheme.onPrimary,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16,
+                ),
+                decoration: InputDecoration(
+                  hintText: strings.namePlaceholder,
+                  hintStyle: theme.textTheme.bodyMedium?.copyWith(
+                    color: theme.colorScheme.onTertiaryContainer,
+                  ),
+                  errorStyle: TextStyle(color: theme.colorScheme.error),
+                  filled: true,
+                  fillColor: theme.colorScheme.tertiaryContainer,
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: Constants.textFieldPaddingHorizontal,
+                    vertical: Constants.textFieldPaddingVertical,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      Constants.textFieldRadius,
+                    ),
+                    borderSide: BorderSide(
+                      color: theme.colorScheme.tertiaryFixedDim,
+                      width: 1,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      Constants.textFieldRadius,
+                    ),
+                    borderSide: BorderSide(
+                      color: theme.colorScheme.tertiaryFixedDim,
+                      width: 1,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      Constants.textFieldRadius,
+                    ),
+                    borderSide: BorderSide(
+                      color: theme.colorScheme.tertiaryFixedDim,
+                      width: 2,
+                    ),
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      Constants.textFieldRadius,
+                    ),
+                    borderSide: BorderSide(
+                      color: theme.colorScheme.error,
+                      width: 1,
+                    ),
+                  ),
+                  focusedErrorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      Constants.textFieldRadius,
+                    ),
+                    borderSide: BorderSide(
+                      color: theme.colorScheme.error,
+                      width: 2,
+                    ),
+                  ),
+                ),
+                validator: _getNameErrorText,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+              ),
+              SizedBox(height: Constants.textFieldSpacing),
+              PrimaryButton(
+                label: strings.continues,
+                disabled: !_isNameValid,
+                onPressed: () {
+                  context.router.replace(const HomeRoute());
+                },
+              ),
             ],
-            center: Alignment.topCenter,
-            stops: [0.3, 1.0],
-            radius: 2.3,
-          ),
-        ),
-        child: SafeArea(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                Text(strings.welcome, style: theme.textTheme.titleLarge),
-                Text(strings.enterYourName, style: theme.textTheme.bodyMedium),
-                Spacer(),
-                TextFormField(
-                  controller: _nameController,
-                  style: theme.textTheme.bodyMedium?.copyWith(
-                    color: theme.colorScheme.onPrimary,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 16,
-                  ),
-                  decoration: InputDecoration(
-                    hintText: strings.namePlaceholder,
-                    hintStyle: theme.textTheme.bodyMedium?.copyWith(
-                      color: theme.colorScheme.onTertiaryContainer,
-                    ),
-                    errorStyle: TextStyle(color: theme.colorScheme.error),
-                    filled: true,
-                    fillColor: theme.colorScheme.tertiaryContainer,
-                    contentPadding: const EdgeInsets.symmetric(
-                      horizontal: Constants.textFieldPaddingHorizontal,
-                      vertical: Constants.textFieldPaddingVertical,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(
-                        Constants.textFieldRadius,
-                      ),
-                      borderSide: BorderSide(
-                        color: theme.colorScheme.tertiaryFixedDim,
-                        width: 1,
-                      ),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(
-                        Constants.textFieldRadius,
-                      ),
-                      borderSide: BorderSide(
-                        color: theme.colorScheme.tertiaryFixedDim,
-                        width: 1,
-                      ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(
-                        Constants.textFieldRadius,
-                      ),
-                      borderSide: BorderSide(
-                        color: theme.colorScheme.tertiaryFixedDim,
-                        width: 2,
-                      ),
-                    ),
-                    errorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(
-                        Constants.textFieldRadius,
-                      ),
-                      borderSide: BorderSide(
-                        color: theme.colorScheme.error,
-                        width: 1,
-                      ),
-                    ),
-                    focusedErrorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(
-                        Constants.textFieldRadius,
-                      ),
-                      borderSide: BorderSide(
-                        color: theme.colorScheme.error,
-                        width: 2,
-                      ),
-                    ),
-                  ),
-                  validator: _getNameErrorText,
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                ),
-                SizedBox(height: Constants.textFieldSpacing),
-                PrimaryButton(
-                  label: strings.continues,
-                  disabled: !_isNameValid,
-                  onPressed: () {
-                    context.router.replace(const HomeRoute());
-                  },
-                ),
-              ],
-            ),
           ),
         ),
       ),
