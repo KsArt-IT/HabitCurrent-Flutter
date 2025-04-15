379f2d04e0f1930527ceedd715d9181f8b6d8248
KsArT-IT <ksart.it@gmail.com>
2025-04-15 19:06:59 +0300

    feat: add Podfile and update xcconfig files for CocoaPods integration

--- app/lib/core/di/service_locator.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get_it/get_it.dart';
-import 'package:habit_current/core/router/app_router.dart';
-
-final getIt = GetIt.instance;
-
-void setupServiceLocator() {
-  // AppRouter
-  getIt.registerLazySingleton<AppRouter>(() => AppRouter());
-}
