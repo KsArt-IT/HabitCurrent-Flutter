f6a001f4d5231bf45abbe1146b8dcff6c9dabbf0
KsArT-IT <ksart.it@gmail.com>
2025-04-17 17:26:15 +0300

    feat: add Users table

---
+++ app/lib/data/database/table/user_table.dart
@@ -0,0 +1,10 @@
+import 'package:drift/drift.dart';
+
+class Users extends Table {
+  IntColumn get id => integer().autoIncrement()();
+
+  TextColumn get name => text().unique()();
+  TextColumn get avatar => text().nullable()();
+  DateTimeColumn get created =>
+      dateTime().clientDefault(() => DateTime.now())();
+}
