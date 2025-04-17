ed21a29a753fa700f9891f3759d237599123c405
KsArT-IT <ksart.it@gmail.com>
2025-04-17 19:53:18 +0300

    feat: add domain to model and model to domain mappers

---
+++ app/lib/data/mappers/domain_to_model.dart
@@ -0,0 +1,47 @@
+import 'package:habit_current/data/models/models.dart';
+import 'package:habit_current/models/habit.dart';
+import 'package:habit_current/models/hour_interval.dart';
+import 'package:habit_current/models/hour_interval_completed.dart';
+import 'package:habit_current/models/user.dart';
+import 'package:habit_current/models/weekdays.dart';
+
+extension UserToModel on User {
+  UserModel toModel() => UserModel(
+    id: id,
+    name: name,
+    avatar: avatar,
+    created: created, //
+  );
+}
+
+extension HabitToModel on Habit {
+  HabitModel toModel() => HabitModel(
+    id: id,
+    userId: userId,
+    name: name,
+    details: details,
+    created: created,
+    updated: updated,
+    completed: completed,
+    weekDaysRaw: weekDays.toInt(),
+    intervals: intervals.map((e) => e.toModel(id)).toList(),
+    completedIntervals: completedIntervals.map((e) => e.toModel(id)).toList(),
+  );
+}
+
+extension HourIntervalToModel on HourInterval {
+  HourIntervalModel toModel(int habitId) => HourIntervalModel(
+    id: id,
+    habitId: habitId,
+    time: time, //
+  );
+}
+
+extension HourIntervalCompletedToModel on HourIntervalCompleted {
+  HourIntervalCompletedModel toModel(int habitId) => HourIntervalCompletedModel(
+    id: id,
+    habitId: habitId,
+    time: time,
+    completed: completed,
+  );
+}
---
+++ app/lib/data/mappers/model_to_domain.dart
@@ -0,0 +1,53 @@
+import 'package:habit_current/data/models/models.dart';
+import 'package:habit_current/models/habit.dart';
+import 'package:habit_current/models/hour_interval.dart';
+import 'package:habit_current/models/hour_interval_completed.dart';
+import 'package:habit_current/models/user.dart';
+import 'package:habit_current/models/weekdays.dart';
+
+extension UserToDomain on UserModel {
+  User toDomain() {
+    return User(
+      id: id,
+      name: name,
+      avatar: avatar,
+      created: created, //
+    );
+  }
+}
+
+extension HabitToDomain on HabitModel {
+  Habit toDomain() {
+    return Habit(
+      id: id,
+      userId: userId,
+      name: name,
+      details: details,
+      created: created,
+      updated: updated,
+      completed: completed,
+      weekDays: WeekDays.fromInt(weekDaysRaw),
+      intervals: intervals.map((e) => e.toDomain()).toList(),
+      completedIntervals: completedIntervals.map((e) => e.toDomain()).toList(),
+    );
+  }
+}
+
+extension HourIntervalToDomain on HourIntervalModel {
+  HourInterval toDomain() {
+    return HourInterval(
+      id: id,
+      time: time, //
+    );
+  }
+}
+
+extension HourIntervalCompletedToDomain on HourIntervalCompletedModel {
+  HourIntervalCompleted toDomain() {
+    return HourIntervalCompleted(
+      id: id,
+      time: time,
+      completed: completed, //
+    );
+  }
+}
