bc647a42b9745803a0f7c8b973734b5c8c069227
KsArT-IT <ksart.it@gmail.com>
2025-04-17 17:31:12 +0300

    feat: create HourIntervalModel for habit tracking

---
+++ app/lib/data/models/hour_interval_model.dart
@@ -0,0 +1,21 @@
+final class HourIntervalModel {
+  final int id;
+  final int habitId;
+  final int time;
+
+  const HourIntervalModel({
+    required this.id,
+    required this.habitId,
+    required this.time,
+  });
+
+  HourIntervalModel copyWith({
+    int? id,
+    int? habitId,
+    int? time, //
+  }) => HourIntervalModel(
+    id: id ?? this.id,
+    habitId: habitId ?? this.habitId,
+    time: time ?? this.time,
+  );
+}
