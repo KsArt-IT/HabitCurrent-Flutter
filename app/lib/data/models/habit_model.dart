27385dab80e8fb4c6e6010d8585d0621503fe658
KsArT-IT <ksart.it@gmail.com>
2025-04-17 19:52:49 +0300

    refactor: update HabitModel to use final fields and improve structure

--- app/lib/data/models/habit_model.dart
+++ app/lib/data/models/habit_model.dart
@@ -1,16 +1,54 @@
-import 'package:drift/drift.dart';
-import 'package:habit_current/models/habit.dart';
+import 'package:habit_current/data/models/hour_interval_completed_model.dart';
+import 'package:habit_current/data/models/hour_interval_model.dart';
 
-@UseRowClass(Habit)
-class HabitModel extends Table {
-  IntColumn get id => integer().autoIncrement()();
-  IntColumn get userId => integer()();
-  TextColumn get name => text().withLength(min: 1, max: 50)();
-  TextColumn get details => text().nullable()();
-  DateTimeColumn get created => dateTime()();
-  DateTimeColumn get updated => dateTime()();
-  DateTimeColumn get completed => dateTime().nullable()();
-  IntColumn get weekDaysRaw => integer()();
-  IntColumn get intervals => integer()();
-  IntColumn get completedIntervals => integer()();
+final class HabitModel {
+  final int id;
+  final int userId;
+
+  final String name;
+  final String? details;
+  final DateTime created;
+  final DateTime? updated;
+  final DateTime? completed;
+
+  final int weekDaysRaw;
+  final List<HourIntervalModel> intervals;
+  final List<HourIntervalCompletedModel> completedIntervals;
+
+  const HabitModel({
+    required this.id,
+    required this.userId,
+    required this.name,
+    this.details,
+    required this.created,
+    this.updated,
+    this.completed,
+    required this.weekDaysRaw,
+    required this.intervals,
+    required this.completedIntervals,
+  });
+
+  HabitModel copyWith({
+    int? id,
+    int? userId,
+    String? name,
+    String? details,
+    DateTime? created,
+    DateTime? updated,
+    DateTime? completed,
+    int? weekDaysRaw,
+    List<HourIntervalModel>? intervals,
+    List<HourIntervalCompletedModel>? completedIntervals,
+  }) => HabitModel(
+    id: id ?? this.id,
+    userId: userId ?? this.userId,
+    name: name ?? this.name,
+    details: details ?? this.details,
+    created: created ?? this.created,
+    updated: updated ?? this.updated,
+    completed: completed ?? this.completed,
+    weekDaysRaw: weekDaysRaw ?? this.weekDaysRaw,
+    intervals: intervals ?? this.intervals,
+    completedIntervals: completedIntervals ?? this.completedIntervals,
+  );
 }
