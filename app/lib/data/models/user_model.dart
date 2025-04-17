246cad5bb34438a16fac2a8e2c3bdc1776326997
KsArT-IT <ksart.it@gmail.com>
2025-04-17 17:27:34 +0300

    refactor: update UserModel

--- app/lib/data/models/user_model.dart
+++ app/lib/data/models/user_model.dart
@@ -1,9 +1,22 @@
-import 'package:drift/drift.dart';
 import 'package:habit_current/models/user.dart';
 
-@UseRowClass(User)
-class UserModel extends Table {
-  IntColumn get id => integer().autoIncrement()();
-  TextColumn get name => text().unique()();
-  TextColumn get avatar => text()();
+final class UserModel {
+  final int id;
+  final String name;
+  final String? avatar;
+  final DateTime created;
+
+  UserModel({
+    required this.id,
+    required this.name,
+    this.avatar,
+    required this.created,
+  });
+
+  User copyWith({String? name, String? avatar, DateTime? created}) => User(
+    id: id,
+    name: name ?? this.name,
+    avatar: avatar ?? this.avatar,
+    created: created ?? this.created,
+  );
 }
