75ccc8b6b2b6a85b159e75f84fa95cd4468175b9
KsArT-IT <ksart.it@gmail.com>
2025-04-21 20:35:21 +0300

    refactor: add borderRadius constant

--- app/lib/core/constants/constants.dart
+++ app/lib/core/constants/constants.dart
@@ -13,6 +13,8 @@ abstract final class Constants {
   static const double paddingLarge = 24;
   static const double paddingXLarge = 32;
 
+  static const double borderRadius = 24;
+
   static final regExpName = RegExp(r'^[іІїЇєЄґҐʼа-яА-Яa-zA-Z\s]+$');
   static final regExpHabit = RegExp(r'^[іІїЇєЄґҐʼа-яА-Яa-zA-Z0-9\s]+$');
 }
