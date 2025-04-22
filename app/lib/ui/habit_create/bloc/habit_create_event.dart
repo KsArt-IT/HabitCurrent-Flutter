ec48bac57da0928feaff29dac1499c233088dab5
KsArT-IT <ksart.it@gmail.com>
2025-04-22 19:59:46 +0300

    refactor: update NameChangedEvent structure

--- app/lib/ui/habit_create/bloc/habit_create_event.dart
+++ app/lib/ui/habit_create/bloc/habit_create_event.dart
@@ -5,16 +5,47 @@ sealed class HabitCreateEvent extends Equatable {
 
   @override
   List<Object?> get props => [];
-
 }
 
-final class HabitCreateSaveEvent extends HabitCreateEvent {}
+final class SubmitHabitEvent extends HabitCreateEvent {}
 
-final class HabitCreateNameChangedEvent extends HabitCreateEvent {
+final class NameChangedEvent extends HabitCreateEvent {
   final String name;
 
-  const HabitCreateNameChangedEvent({required this.name});
+  const NameChangedEvent({required this.name});
 
   @override
   List<Object?> get props => [name];
+}
+
+// Daily or Week days
+final class DailyOrWeekToggledEvent extends HabitCreateEvent {
+  final Frequency frequency;
+
+  const DailyOrWeekToggledEvent(Frequency? value)
+    : frequency = value ?? Frequency.daily;
+
+  @override
+  List<Object?> get props => [frequency];
+}
+
+final class WeekDayChangedEvent extends HabitCreateEvent {
+  final WeekDays weekDay;
+
+  const WeekDayChangedEvent(this.weekDay);
+
+  @override
+  List<Object?> get props => [weekDay];
+}
+
+// Hour intervals
+class TimeAddedEvent extends HabitCreateEvent {}
+
+class TimeRemovedEvent extends HabitCreateEvent {}
+
+final class ReminderToggledEvent extends HabitCreateEvent {
+  final bool enabled;
+  const ReminderToggledEvent(this.enabled);
+  @override
+  List<Object?> get props => [enabled];
 }
