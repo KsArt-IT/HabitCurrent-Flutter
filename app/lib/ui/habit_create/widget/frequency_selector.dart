a5fbc440409f6dc306b77f54c47c19d6a81ed971
KsArT-IT <ksart.it@gmail.com>
2025-04-22 19:56:54 +0300

    refactor: change FrequencySelector to use radioListTile

--- app/lib/ui/habit_create/widget/frequency_selector.dart
+++ app/lib/ui/habit_create/widget/frequency_selector.dart
@@ -3,10 +3,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:habit_current/core/constants/constants.dart';
 import 'package:habit_current/l10n/intl_exp.dart';
 import 'package:habit_current/ui/habit_create/bloc/habit_create_bloc.dart';
+import 'package:habit_current/ui/widgets/text_form_title.dart';
 
 class FrequencySelector extends StatelessWidget {
-  const FrequencySelector({super.key, required this.selector});
-  final Widget selector;
+  const FrequencySelector({super.key, required this.selectorDays});
+  final Widget selectorDays;
 
   @override
   Widget build(BuildContext context) {
@@ -16,10 +17,37 @@ class FrequencySelector extends StatelessWidget {
     final strings = context.l10n;
     final theme = Theme.of(context);
 
+    RadioListTile radioListTile({
+      required String title,
+      required Frequency value,
+    }) {
+      return RadioListTile(
+        title: Text(
+          title,
+          style:
+              frequency == value
+                  ? theme.textTheme.titleSmall?.copyWith(
+                    color: theme.colorScheme.primaryFixed,
+                  )
+                  : theme.textTheme.titleSmall,
+        ),
+        visualDensity: VisualDensity.compact,
+        dense: true,
+        contentPadding: const EdgeInsets.all(0),
+        controlAffinity: ListTileControlAffinity.leading,
+        radioScaleFactor: 1.2,
+        value: value,
+        groupValue: frequency,
+        onChanged: (value) {
+          context.read<HabitCreateBloc>().add(DailyOrWeekToggledEvent(value));
+        },
+      );
+    }
+
     return Container(
       padding: const EdgeInsets.all(Constants.paddingMedium),
       decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.surface,
+        color: theme.colorScheme.surface,
         borderRadius: BorderRadius.circular(Constants.borderRadius),
       ),
 
@@ -28,53 +56,13 @@ class FrequencySelector extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(strings.howOften, style: theme.textTheme.titleMedium),
+          TextFormTitle(strings.howOften),
           const SizedBox(height: Constants.paddingMedium),
-          RadioListTile(
-            title: Text(
-              strings.daily,
-              style:
-                  frequency == Frequency.daily
-                      ? theme.textTheme.titleSmall?.copyWith(
-                        color: theme.colorScheme.primaryFixed,
-                      )
-                      : theme.textTheme.titleSmall,
-            ),
-            contentPadding: const EdgeInsets.all(0),
-            controlAffinity: ListTileControlAffinity.leading,
-            radioScaleFactor: 1.2,
-            value: Frequency.daily,
-            groupValue: frequency,
-            onChanged: (value) {
-              context.read<HabitCreateBloc>().add(
-                DailyOrWeekToggledEvent(value),
-              );
-            },
-          ),
-          RadioListTile(
-            title: Text(
-              strings.yourSchedule,
-              style:
-                  frequency == Frequency.weekly
-                      ? theme.textTheme.titleSmall?.copyWith(
-                        color: theme.colorScheme.primaryFixed,
-                      )
-                      : theme.textTheme.titleSmall,
-            ),
-            contentPadding: const EdgeInsets.all(0),
-            controlAffinity: ListTileControlAffinity.leading,
-            radioScaleFactor: 1.2,
-            value: Frequency.weekly,
-            groupValue: frequency,
-            onChanged: (value) {
-              context.read<HabitCreateBloc>().add(
-                DailyOrWeekToggledEvent(value),
-              );
-            },
-          ),
+          radioListTile(title: strings.daily, value: Frequency.daily),
+          radioListTile(title: strings.yourSchedule, value: Frequency.weekly),
           if (frequency == Frequency.weekly)
             const SizedBox(height: Constants.paddingMedium),
-          if (frequency == Frequency.weekly) selector,
+          if (frequency == Frequency.weekly) selectorDays,
         ],
       ),
     );
