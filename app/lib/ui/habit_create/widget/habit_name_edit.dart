9631c98d628c820399c5eea2ee830988de0f1b8c
KsArT-IT <ksart.it@gmail.com>
2025-04-22 19:56:23 +0300

    feat: refactor HabitNameEdit to HabitNameEditWidget

--- app/lib/ui/habit_create/widget/habit_name_edit.dart
+++ app/lib/ui/habit_create/widget/habit_name_edit.dart
@@ -1,51 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:habit_current/core/constants/constants.dart';
-import 'package:habit_current/generated/l10n.dart';
+import 'package:habit_current/l10n/intl_exp.dart';
+import 'package:habit_current/ui/habit_create/bloc/habit_create_bloc.dart';
+import 'package:habit_current/ui/widgets/text_form_title.dart';
 
-class HabitNameEdit extends StatefulWidget {
-  const HabitNameEdit({super.key, required this.onChanged});
-
-  final String initialValue = '';
-  final Function(String) onChanged;
-
-  @override
-  State<HabitNameEdit> createState() => _HabitNameEditState();
-}
-
-class _HabitNameEditState extends State<HabitNameEdit> {
-  TextEditingController _controller = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = TextEditingController();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+class HabitNameEditWidget extends StatelessWidget {
+  const HabitNameEditWidget({super.key,});
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final S strings = S.of(context);
+    final strings = context.l10n;
 
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(Constants.paddingMedium),
-        child: TextField(
-          controller: _controller,
-          decoration: const InputDecoration(
-            border: OutlineInputBorder(),
-            filled: true,
-            // fillColor: theme.colorScheme.surface,
-            // hintText: strings.habitEnterNameHint,
-            // hintStyle: theme.textTheme.bodyMedium,
+    return Container(
+      padding: const EdgeInsets.all(Constants.paddingMedium),
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surface,
+        borderRadius: BorderRadius.circular(Constants.borderRadius),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextFormTitle(strings.habitEnterName),
+          SizedBox(height: Constants.paddingMedium),
+          TextField(
+            style: theme.textTheme.bodyLarge,
+            decoration: InputDecoration(
+              border: const OutlineInputBorder(),
+              filled: true,
+              fillColor: theme.colorScheme.secondaryContainer,
+              hintText: strings.habitEnterNameHint,
+              hintStyle: theme.textTheme.headlineSmall,
+              errorStyle: theme.textTheme.headlineSmall?.copyWith(
+                color: theme.colorScheme.error,
+              ),
+              contentPadding: const EdgeInsets.symmetric(
+                horizontal: Constants.textFieldPaddingHorizontal,
+                vertical: Constants.textFieldPaddingVertical,
+              ),
+            ),
+            onChanged: (value) => context.read<HabitCreateBloc>().add(NameChangedEvent(name: value)),
           ),
-          onChanged: widget.onChanged,
-        ),
+        ],
       ),
     );
   }
