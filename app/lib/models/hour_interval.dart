a022718f14e44f01cb1b901b206bfc2324ee13da
KsArT-IT <ksart.it@gmail.com>
2025-04-22 20:01:02 +0300

    refactor: update HourInterval constructor

--- app/lib/models/hour_interval.dart
+++ app/lib/models/hour_interval.dart
@@ -2,7 +2,7 @@ final class HourInterval {
   final int id;
   final int time;
 
-  const HourInterval({required this.id, required this.time});
+  const HourInterval({this.id = 0, required this.time});
 
   HourInterval copyWith({int? time}) => HourInterval(
     id: this.id,
