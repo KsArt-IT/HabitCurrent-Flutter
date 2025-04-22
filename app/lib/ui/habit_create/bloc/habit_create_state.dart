7ca594836d6832633cc4c7090e3f1019c90b586e
KsArT-IT <ksart.it@gmail.com>
2025-04-22 19:57:16 +0300

    feat: update HabitCreateState to include frequency and reminder

--- app/lib/ui/habit_create/bloc/habit_create_state.dart
+++ app/lib/ui/habit_create/bloc/habit_create_state.dart
@@ -1,36 +1,70 @@
 part of 'habit_create_bloc.dart';
 
-enum StatsStatus { initial, loading, valid, success, failure }  
+enum StatsStatus { initial, loading, valid, success, failure }
+
+enum Frequency { daily, weekly }
 
 final class HabitCreateState extends Equatable {
   final StatsStatus status;
+
   final String name;
   final String? details;
+
+  // Daily or Week days
+  final Frequency frequency;
   final Set<WeekDays> weekDays;
-  final List<HourInterval> intervals;
+  // Hour intervals
+  final List<int> intervals;
+  // ReminderSelector
+  final bool isReminder;
 
   const HabitCreateState({
-    this.status = StatsStatus.initial, 
+    this.status = StatsStatus.initial,
+
     this.name = '',
     this.details,
+
+    this.frequency = Frequency.daily,
     this.weekDays = const {},
-    this.intervals = const [],
+    this.intervals = const [420],
+    this.isReminder = false,
   });
-  
+
   HabitCreateState copyWith({
     StatsStatus? status,
     String? name,
     String? details,
+    Frequency? frequency,
     Set<WeekDays>? weekDays,
-    List<HourInterval>? intervals,
-  }) => HabitCreateState(
-    status: status ?? this.status,
-    name: name ?? this.name,
-    details: details ?? this.details,
-    weekDays: weekDays ?? this.weekDays,
-    intervals: intervals ?? this.intervals,
-  );
+    List<int>? intervals,
+    bool? isReminder,
+  }) {
+    StatsStatus checkStatus = status ?? StatsStatus.valid;
+    if ((name ?? this.name).isEmpty ||
+        (frequency ?? this.frequency) == Frequency.weekly &&
+            (weekDays ?? this.weekDays).isEmpty) {
+      checkStatus = StatsStatus.failure;
+    }
+
+    return HabitCreateState(
+      status: checkStatus,
+      name: name ?? this.name,
+      details: details ?? this.details,
+      frequency: frequency ?? this.frequency,
+      weekDays: weekDays ?? this.weekDays,
+      intervals: intervals ?? this.intervals,
+      isReminder: isReminder ?? this.isReminder,
+    );
+  }
 
   @override
-  List<Object?> get props => [name, details, weekDays, intervals];
+  List<Object?> get props => [
+    status,
+    name,
+    details,
+    frequency,
+    weekDays,
+    intervals,
+    isReminder,
+  ];
 }
