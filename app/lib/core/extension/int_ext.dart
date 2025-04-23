46973f9232cfaa289567521adc5de7efab2fe749
KsArT-IT <ksart.it@gmail.com>
2025-04-23 20:32:46 +0300

    refactor: enhance IntTime extension with toTimeOfDay method

--- app/lib/core/extension/int_ext.dart
+++ app/lib/core/extension/int_ext.dart
@@ -1,7 +1,19 @@
+import 'package:flutter/material.dart';
+
 extension IntTime on int {
   String toTime() {
     final hours = this ~/ 60;
     final minutes = this % 60;
     return "${hours.toString().padLeft(2, '0')}:${minutes.toString().padLeft(2, '0')}";
   }
+
+  TimeOfDay toTimeOfDay() {
+    final hours = this ~/ 60;
+    final minutes = this % 60;
+    return TimeOfDay(hour: hours, minute: minutes);
+  }
+}
+
+extension IntTimeOfDay on TimeOfDay {
+  int toMinutes() => hour * 60 + minute;
 }
