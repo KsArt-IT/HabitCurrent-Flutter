4e8955e9a2a2e3c3598171c53ed5a46ad4d98f49
KsArT-IT <ksart.it@gmail.com>
2025-04-24 18:44:30 +0300

    style: change style for TabItemButton

--- app/lib/ui/home/widget/tab_item_button.dart
+++ app/lib/ui/home/widget/tab_item_button.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:habit_current/core/utils/constants.dart';
+import 'package:habit_current/core/constants/constants.dart';
 import 'package:habit_current/ui/home/home_tab.dart';
 
 class TabItemButton extends StatelessWidget {
@@ -7,27 +7,50 @@ class TabItemButton extends StatelessWidget {
   final int active;
   final Function(int) onTap;
 
-  const TabItemButton({super.key, required this.tab, required this.active, required this.onTap});
+  const TabItemButton({
+    super.key,
+    required this.tab,
+    required this.active,
+    required this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
     final isActive = active == tab.index;
-    final colorScheme = Theme.of(context).colorScheme;
+    final theme = Theme.of(context);
 
-    return SizedBox(
-      width: AppConstants.tabItemButtonSize,
-      height: AppConstants.tabItemButtonSize,
-      child: InkWell(
-        onTap: () => onTap(tab.index),
-        customBorder: const CircleBorder(),
+    return InkWell(
+      onTap: () => onTap(tab.index),
+      customBorder: const CircleBorder(),
+      child: Ink(
+        height: Constants.tabItemButtonSize,
+        width: Constants.tabItemButtonSize,
+        decoration: BoxDecoration(
+          color:
+              isActive
+                  ? theme.colorScheme.inversePrimary
+                  : theme.colorScheme.primary,
+          borderRadius: BorderRadius.circular(Constants.tabItemRadius),
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(tab.icon, color: isActive ? colorScheme.primary : colorScheme.inversePrimary),
+            Icon(
+              tab.icon,
+              color:
+                  isActive
+                      ? theme.colorScheme.primary
+                      : theme.colorScheme.inversePrimary,
+            ),
             Text(
               tab.getLabel(context),
               overflow: TextOverflow.ellipsis,
-              style: TextStyle(color: isActive ? colorScheme.primary : colorScheme.inversePrimary, fontSize: 12),
+              style:
+                  isActive
+                      ? theme.textTheme.headlineMedium!.copyWith(
+                        color: theme.colorScheme.primary,
+                      )
+                      : theme.textTheme.headlineMedium,
             ),
           ],
         ),
