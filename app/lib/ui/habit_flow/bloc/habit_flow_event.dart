58ae43c3b8397c256fe0869d997b84e9357d8a55
KsArT-IT <ksart.it@gmail.com>
2025-04-25 21:41:16 +0300

    feat: add events to manage habit flow in HabitFlowBloc

---
+++ app/lib/ui/habit_flow/bloc/habit_flow_event.dart
@@ -0,0 +1,13 @@
+part of 'habit_flow_bloc.dart';
+
+sealed class HabitFlowEvent {}
+
+class LoadHabitsEvent extends HabitFlowEvent {
+  final int userId;
+
+  LoadHabitsEvent({required this.userId});
+}
+
+class RefreshHabitsEvent extends HabitFlowEvent {}
+
+class HabitCreatedEvent extends HabitFlowEvent {}
