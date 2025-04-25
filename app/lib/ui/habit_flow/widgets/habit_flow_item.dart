50a410ae6c19f63569de49cecc67e521e263e1c4
KsArT-IT <ksart.it@gmail.com>
2025-04-25 23:42:14 +0300

    feat: update HabitFlowItem widget to improve design

--- app/lib/ui/habit_flow/widgets/habit_flow_item.dart
+++ app/lib/ui/habit_flow/widgets/habit_flow_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:habit_current/core/constants/constants.dart';
+import 'package:habit_current/l10n/intl_exp.dart';
 import 'package:habit_current/models/habit.dart';
 
 class HabitFlowItem extends StatelessWidget {
@@ -10,84 +11,88 @@ class HabitFlowItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final strings = context.l10n;
 
-    return Card(
-      child: InkWell(
-        onTap: () {
-          // context.read<HabitFlowBloc>().add(
-          //   CompleteHabitIntervalEvent(habitId: habit.id),
-          // );
-        },
-        borderRadius: BorderRadius.circular(Constants.borderRadius),
-        child: Padding(
-          padding: const EdgeInsets.all(Constants.paddingMedium),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: Text(
-                      habit.name,
-                      style: theme.textTheme.titleMedium,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                  PopupMenuButton<String>(
-                    icon: Icon(
-                      Icons.more_vert,
-                      color: theme.colorScheme.onSurfaceVariant,
-                    ),
-                    itemBuilder:
-                        (context) => [
-                          PopupMenuItem(
-                            value: 'edit',
-                            child: Text('Редактировать'),
-                          ),
-                          PopupMenuItem(
-                            value: 'delete',
-                            child: Text('Удалить'),
-                          ),
-                        ],
-                  ),
-                ],
-              ),
-              const Spacer(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    '${habit.completedIntervals.length}/${habit.intervals.length}',
+    return InkWell(
+      onTap: () {
+        // context.read<HabitFlowBloc>().add(
+        //   CompleteHabitIntervalEvent(habitId: habit.id),
+        // );
+      },
+      borderRadius: BorderRadius.circular(Constants.borderRadius),
+      child: Ink(
+        padding: const EdgeInsets.fromLTRB(
+          Constants.paddingMedium,
+          Constants.paddingMedium,
+          Constants.paddingSmall,
+          Constants.paddingSmall,
+        ),
+        decoration: BoxDecoration(
+          color: theme.colorScheme.surface,
+          borderRadius: BorderRadius.circular(Constants.borderRadius),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Text(
+                    habit.name,
                     style: theme.textTheme.titleMedium,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
                   ),
-                  IconButton(
-                    padding: EdgeInsets.all(16),
-                    color: theme.colorScheme.inversePrimary,
-                    onPressed: () {},
-                    icon: Icon(
-                      size: 24,
-                      Icons.arrow_forward,
-                      color: theme.colorScheme.onPrimaryFixed,
+                ),
+                PopupMenuButton<String>(
+                  icon: Icon(
+                    Icons.more_horiz,
+                    color: theme.colorScheme.onPrimary,
+                    size: Constants.iconSize,
+                  ),
+                  itemBuilder:
+                      (context) => [
+                        PopupMenuItem(
+                          value: 'edit',
+                          child: Text(
+                            strings.edit,
+                            style: theme.textTheme.bodyMedium,
+                          ),
+                        ),
+                        PopupMenuItem(
+                          value: 'delete',
+                          child: Text(
+                            strings.delete,
+                            style: theme.textTheme.bodyMedium,
+                          ),
+                        ),
+                      ],
+                ),
+              ],
+            ),
+            const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '${habit.completedIntervals.length}/${habit.intervals.length}',
+                  style: theme.textTheme.displayMedium,
+                ),
+                IconButton(
+                  padding: EdgeInsets.all(Constants.paddingMedium),
+                  onPressed: () {},
+                  color: theme.colorScheme.onPrimaryFixed,
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.all(
+                      theme.colorScheme.secondaryFixed,
                     ),
                   ),
-                  // Container(
-                  //   width: 40,
-                  //   height: 40,
-                  //   decoration: BoxDecoration(
-                  //     color: theme.colorScheme.primaryContainer,
-                  //     shape: BoxShape.circle,
-                  //   ),
-                  //   child: Icon(
-                  //     Icons.arrow_forward,
-                  //     color: theme.colorScheme.onPrimaryContainer,
-                  //   ),
-                  // ),
-                ],
-              ),
-            ],
-          ),
+                  icon: Icon(size: Constants.iconSize, Icons.north_east),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
