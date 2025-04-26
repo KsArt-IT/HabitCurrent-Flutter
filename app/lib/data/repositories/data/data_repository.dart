a4f415ab3cd3805d14837ad114550177307a728b
KsArT-IT <ksart.it@gmail.com>
2025-04-26 21:50:01 +0300

    feat(data_repository): add methods for working with HourIntervalCompleted

--- app/lib/data/repositories/data/data_repository.dart
+++ app/lib/data/repositories/data/data_repository.dart
@@ -1,4 +1,5 @@
 import 'package:habit_current/models/habit.dart';
+import 'package:habit_current/models/hour_interval_completed.dart';
 import 'package:habit_current/models/user.dart';
 
 abstract interface class DataRepository {
@@ -9,9 +10,13 @@ abstract interface class DataRepository {
 
   Future<List<Habit>> loadHabitsByUserId(int userId);
   Future<List<Habit>> loadHabitsByUserIdFromDate(int userId, DateTime date);
+  Future<Habit?> loadHabitById(int id, DateTime date);
   Future<Habit> createHabit(Habit habit);
   Future<void> saveHabit(Habit habit);
   Future<void> deleteHabitById(int id);
+
+  Future<HourIntervalCompleted> createHourIntervalCompleted(int habitId, HourIntervalCompleted interval);
+  Future<void> deleteHourIntervalCompletedById(int id);
 
   Future<void> close();
 }
