2b1b146ce5e49326bcef39c7cc3fa66855960237
KsArT-IT <ksart.it@gmail.com>
2025-04-29 16:54:57 +0300

    feat(habit_edit): implement TimeContainer widget for displaying and selecting time

---
+++ app/lib/ui/habit_edit/widget/time_container.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:habit_current/core/constants/constants.dart';
+import 'package:habit_current/core/extension/int_ext.dart';
+
+class TimeContainer extends StatelessWidget {
+  const TimeContainer({super.key, required this.time, this.onTap});
+
+  final int time;
+  final GestureTapCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Center(
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: onTap,
+          borderRadius: BorderRadius.circular(Constants.timeContainerRadius),
+          child: Ink(
+            height: Constants.timeContainerHeight,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: theme.colorScheme.secondaryContainer,
+              borderRadius: BorderRadius.circular(
+                Constants.timeContainerRadius,
+              ),
+            ),
+            child: Center(
+              child: Text(time.toTime(), style: theme.textTheme.labelSmall),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
