e666ecf5e785b26e396bb57b3837c506c91305ba
KsArT-IT <ksart.it@gmail.com>
2025-04-30 21:14:02 +0300

    feat(habit_week_screen): implement HabitWeekScreen

--- app/lib/ui/habit_week/habit_week_screen.dart
+++ app/lib/ui/habit_week/habit_week_screen.dart
@@ -1,5 +1,12 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:habit_current/app/bloc/app_bloc.dart';
+import 'package:habit_current/core/constants/constants.dart';
+import 'package:habit_current/l10n/intl_exp.dart';
+import 'package:habit_current/models/habit_status.dart';
+import 'package:habit_current/ui/habit_week/bloc/habit_week_bloc.dart';
+import 'package:habit_current/ui/habit_week/widgets/habit_week_card.dart';
 
 @RoutePage()
 class HabitWeekScreen extends StatelessWidget {
@@ -7,12 +14,75 @@ class HabitWeekScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('Week', style: Theme.of(context).textTheme.headlineMedium),
-        ],
+    final appState = context.read<AppBloc>().state;
+
+    int userId = 0;
+    if (appState is AppLoadedState) {
+      userId = appState.user.id;
+    }
+
+    return BlocProvider(
+      create:
+          (context) =>
+              HabitWeekBloc(repository: context.read())
+                ..add(LoadHabitWeekEvent(userId: userId)),
+      child: const _HabitWeekBody(),
+    );
+  }
+}
+
+class _HabitWeekBody extends StatelessWidget {
+  const _HabitWeekBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final strings = context.l10n;
+
+    return BlocListener<AppBloc, AppState>(
+      listener: (context, state) {
+        if (state is AppHabitReloadState) {
+          context.read<HabitWeekBloc>().add(
+            ReloadHabitEvent(habitId: state.habitId),
+          );
+        }
+      },
+      child: RefreshIndicator(
+        color: theme.colorScheme.primaryFixed,
+        onRefresh: () async {
+          context.read<HabitWeekBloc>().add(
+            RefreshHabitWeekEvent(DateTime.now()),
+          );
+        },
+        child: BlocBuilder<HabitWeekBloc, HabitWeekState>(
+          builder: (context, state) {
+            if (state.status == HabitStatus.loading) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            if (state.completedHabits.isEmpty) {
+              return Center(
+                child: Text(strings.noHabits, style: theme.textTheme.bodyLarge),
+              );
+            }
+
+            return ListView.builder(
+              padding: const EdgeInsets.fromLTRB(
+                Constants.paddingMedium,
+                Constants.paddingSmall,
+                Constants.paddingMedium,
+                Constants.paddingXLarge,
+              ),
+              itemCount: state.completedHabits.length,
+              itemBuilder: (context, index) {
+                return HabitWeekCard(
+                  name: state.completedHabits[index].habit.name,
+                  weekStatus: state.completedHabits[index].weekStatus,
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
