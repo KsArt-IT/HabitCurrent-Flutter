2bf1da862cfd17512dbfff71f69b9cc0cef03ec3
KsArT-IT <ksart.it@gmail.com>
2025-04-30 16:58:11 +0300

    feat(weekdays): add getShortDayName method and update fromDate logic

--- app/lib/models/weekdays.dart
+++ app/lib/models/weekdays.dart
@@ -28,11 +28,32 @@ enum WeekDays {
     WeekDays.saturday => AppLocalizations.of(context).saturday,
     WeekDays.sunday => AppLocalizations.of(context).sunday,
   };
-  static WeekDays get today => WeekDays.values[DateTime.now().weekday - 1];
+  String getShortDayName(BuildContext context) => switch (this) {
+    WeekDays.monday => AppLocalizations.of(context).mondayShort,
+    WeekDays.tuesday => AppLocalizations.of(context).tuesdayShort,
+    WeekDays.wednesday => AppLocalizations.of(context).wednesdayShort,
+    WeekDays.thursday => AppLocalizations.of(context).thursdayShort,
+    WeekDays.friday => AppLocalizations.of(context).fridayShort,
+    WeekDays.saturday => AppLocalizations.of(context).saturdayShort,
+    WeekDays.sunday => AppLocalizations.of(context).sundayShort,
+  };
   static const int none = 0;
   static const int allDays = 127;
   static Set<WeekDays> fromInt(int value) =>
       WeekDays.values.where((element) => value & element.toBit != 0).toSet();
+  static WeekDays fromDate([DateTime? date]) {
+    final day = (date ?? DateTime.now()).weekday;
+    return switch (day) {
+      1 => WeekDays.monday,
+      2 => WeekDays.tuesday,
+      3 => WeekDays.wednesday,
+      4 => WeekDays.thursday,
+      5 => WeekDays.friday,
+      6 => WeekDays.saturday,
+      7 => WeekDays.sunday,
+      _ => throw ArgumentError('Invalid weekday'),
+    };
+  }
 }
 
 extension Raw on Set<WeekDays> {
