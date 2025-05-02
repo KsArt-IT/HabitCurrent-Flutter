3a09728a13f17810550ea7b150bb7e37a63a3a86
KsArT-IT <ksart.it@gmail.com>
2025-05-02 20:01:22 +0300

    feat(habit_month_screen): implement HabitMonthScreen with BlocProvider and loading state management

--- app/lib/ui/habit_month/habit_month_screen.dart
+++ app/lib/ui/habit_month/habit_month_screen.dart
@@ -1,5 +1,13 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:habit_current/app/bloc/app_bloc.dart';
+import 'package:habit_current/core/constants/constants.dart';
+import 'package:habit_current/l10n/intl_exp.dart';
+import 'package:habit_current/models/habit_state_status.dart';
+import 'package:habit_current/ui/habit_month/bloc/habit_month_bloc.dart';
+import 'package:habit_current/ui/habit_month/widgets/habit_month_card.dart';
+import 'package:habit_current/ui/habit_month/widgets/month_selector.dart';
 
 @RoutePage()
 class HabitMonthScreen extends StatelessWidget {
@@ -7,12 +15,83 @@ class HabitMonthScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('Month', style: Theme.of(context).textTheme.headlineMedium),
-        ],
+    final appState = context.read<AppBloc>().state;
+
+    int userId = 0;
+    if (appState is AppLoadedState) {
+      userId = appState.user.id;
+    }
+
+    return BlocProvider(
+      create:
+          (_) =>
+              HabitMonthBloc(repository: context.read())
+                ..add(LoadHabitEvent(userId: userId)),
+      child: const _HabitMonthBody(),
+    );
+  }
+}
+
+class _HabitMonthBody extends StatelessWidget {
+  const _HabitMonthBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final strings = context.l10n;
+
+    return BlocListener<AppBloc, AppState>(
+      listener: (context, state) {
+        if (state is AppHabitReloadState) {
+          context.read<HabitMonthBloc>().add(
+            ReloadHabitEvent(habitId: state.habitId),
+          );
+        }
+      },
+      child: BlocBuilder<HabitMonthBloc, HabitMonthState>(
+        builder: (context, state) {
+          if (state.status == HabitStateStatus.loading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          return Column(
+            children: [
+              MonthSelector(),
+              // TODO: тут 2 надписи и статус передать за этот месяц нет или вобще нет привычек
+              if (state.statusHabits.isEmpty)
+                Center(
+                  child: Text(
+                    strings.noHabits,
+                    style: theme.textTheme.bodyLarge,
+                  ),
+                ),
+              if (state.statusHabits.isNotEmpty)
+                Expanded(
+                  child: RefreshIndicator(
+                    color: theme.colorScheme.primaryFixed,
+                    onRefresh: () async {
+                      context.read<HabitMonthBloc>().add(
+                        RefreshHabitEvent(DateTime.now()),
+                      );
+                    },
+                    child: ListView.builder(
+                      padding: const EdgeInsets.fromLTRB(
+                        Constants.paddingMedium,
+                        Constants.paddingSmall,
+                        Constants.paddingMedium,
+                        Constants.paddingXLarge,
+                      ),
+                      itemCount: state.statusHabits.length,
+                      itemBuilder:
+                          (_, index) => HabitMonthCard(
+                            habitMonth: state.statusHabits[index],
+                          ),
+                    ),
+                  ),
+                ),
+            ],
+          );
+        },
       ),
     );
   }
