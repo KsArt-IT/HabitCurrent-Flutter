a823c126c26dc24922c58d339cba71e62516bd25
KsArT-IT <ksart.it@gmail.com>
2025-05-02 22:46:35 +0300

    feat(month_selector): replace IconButton with ArrowButton for month navigation

--- app/lib/ui/habit_month/widgets/month_selector.dart
+++ app/lib/ui/habit_month/widgets/month_selector.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:habit_current/ui/habit_month/bloc/habit_month_bloc.dart';
-import 'package:intl/intl.dart';
+import 'package:habit_current/ui/habit_month/widgets/month_name_widget.dart';
+import 'package:habit_current/ui/widgets/arrow_button.dart';
 
 class MonthSelector extends StatelessWidget {
   const MonthSelector({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-
     return BlocBuilder<HabitMonthBloc, HabitMonthState>(
-      buildWhen: (previous, current) => previous.selectedMonth != current.selectedMonth,
+      buildWhen:
+          (previous, current) =>
+              previous.selectedMonth != current.selectedMonth,
       builder: (context, state) {
         final selectedMonth = state.selectedMonth;
         final previousMonth = DateTime(
@@ -26,65 +27,26 @@ class MonthSelector extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           mainAxisSize: MainAxisSize.max,
           children: [
-            IconButton(
-              color: theme.colorScheme.onPrimary,
-              icon: const Icon(Icons.chevron_left),
-              onPressed:
-                  isFirstMonth
-                      ? null
-                      : () {
-                        context.read<HabitMonthBloc>().add(
-                          ChangeMonthHabitEvent(previousMonth),
-                        );
-                      },
+            ArrowButton(
+              left: true,
+              disabled: isFirstMonth,
+              onPressed: () {
+                context.read<HabitMonthBloc>().add(
+                  ChangeMonthHabitEvent(previousMonth),
+                );
+              },
             ),
-            if (isFirstMonth) const SizedBox(width: 50)
-            else
-              SizedBox(
-                width: 50,
-                child: Text(
-                  DateFormat('MMM').format(previousMonth),
-                  style: theme.textTheme.titleMedium?.copyWith(
-                    color: theme.colorScheme.onTertiaryFixed,
-                    fontWeight: FontWeight.w300,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            SizedBox(
-              width:50,
-              child: Text(
-                DateFormat('MMM').format(selectedMonth),
-                style: theme.textTheme.titleMedium?.copyWith(
-                  color: theme.colorScheme.onPrimary,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            if (isLastMonth) const SizedBox(width: 50)
-            else
-              SizedBox(
-                width: 50,
-                child: Text(
-                  DateFormat('MMM').format(nextMonth),
-                  style: theme.textTheme.titleMedium?.copyWith(
-                    color: theme.colorScheme.onTertiaryFixed,
-                  fontWeight: FontWeight.w300,
-                ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            IconButton(
-              color: theme.colorScheme.onPrimary,
-              icon: const Icon(Icons.chevron_right),
-              onPressed:
-                  isLastMonth
-                      ? null
-                      : () {
-                        context.read<HabitMonthBloc>().add(
-                          ChangeMonthHabitEvent(nextMonth),
-                        );
-                      },
+            MonthNameWidget(month: previousMonth, disabled: isFirstMonth),
+            MonthNameWidget(month: selectedMonth, selected: true),
+            MonthNameWidget(month: nextMonth, disabled: isLastMonth),
+            ArrowButton(
+              left: false,
+              disabled: isLastMonth,
+              onPressed: () {
+                context.read<HabitMonthBloc>().add(
+                  ChangeMonthHabitEvent(nextMonth),
+                );
+              },
             ),
           ],
         );
