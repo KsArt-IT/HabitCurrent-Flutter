79f2aeb56f5a7f8af98a0f4630f28c9bdbef4775
KsArT-IT <ksart.it@gmail.com>
2025-05-02 20:00:14 +0300

    feat(month_status_widget): add MonthStatusWidget for displaying weekly habit statuses

---
+++ app/lib/ui/habit_month/widgets/month_status_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:habit_current/core/constants/constants.dart';
+import 'package:habit_current/models/habit_day_status.dart';
+import 'package:habit_current/ui/habit_week/widgets/habit_status_widget.dart';
+
+class MonthStatusWidget extends StatelessWidget {
+  final List<List<HabitDayStatus>> weeksStatus;
+
+  const MonthStatusWidget({super.key, required this.weeksStatus});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children:
+          weeksStatus.map((week) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: Constants.paddingSmaller,
+              ),
+              child: HabitStatusWidget(weekStatus: week),
+            );
+          }).toList(),
+    );
+  }
+}
