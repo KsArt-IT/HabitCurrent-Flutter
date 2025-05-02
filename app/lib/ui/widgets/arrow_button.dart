ff47465f4e6a3e4e99589e2430d5a7de69b82f15
KsArT-IT <ksart.it@gmail.com>
2025-05-02 22:46:03 +0300

    feat(arrow_button): add ArrowButton widget to change month

---
+++ app/lib/ui/widgets/arrow_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ArrowButton extends StatelessWidget {
+  final bool left;
+  final bool disabled;
+  final void Function() onPressed;
+  const ArrowButton({
+    super.key,
+    this.left = true,
+    this.disabled = false,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return IconButton(
+      color: theme.colorScheme.onPrimary,
+      icon:
+          left
+              ? const Icon(Icons.chevron_left)
+              : const Icon(Icons.chevron_right),
+      onPressed: disabled ? null : onPressed,
+    );
+  }
+}
