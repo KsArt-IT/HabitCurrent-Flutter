4b88e6eb14027d31e0b2a17eb27e6cac539f92b8
KsArT-IT <ksart.it@gmail.com>
2025-05-02 18:45:00 +0300

    refactor(datetime_ext): streamline toWeekRange method and tidy up formatting

--- app/lib/core/extension/datetime_ext.dart
+++ app/lib/core/extension/datetime_ext.dart
@@ -1,20 +1,19 @@
 typedef WeekRange = ({DateTime start, DateTime end});
+typedef MonthRange = ({DateTime start, DateTime end});
 
 extension IntWeekDay on DateTime {
   DateTime toEndOfDay() => DateTime(year, month, day, 23, 59, 59, 999);
-
+  DateTime toStartOfDay() => DateTime(year, month, day, 0, 0, 0, 0);
+  
   WeekRange toWeekRange() {
     final DateTime startOfWeek = subtract(Duration(days: weekday - 1));
     final DateTime endOfWeek = startOfWeek.add(const Duration(days: 6));
+    return (start: startOfWeek.toEndOfDay(), end: endOfWeek.toEndOfDay());
+  }
 
-    final DateTime start = DateTime(
-      startOfWeek.year,
-      startOfWeek.month,
-      startOfWeek.day,
-    );
-
-    final DateTime end = endOfWeek.toEndOfDay();
-
+  MonthRange toMonthRange() {
+    final start = DateTime(year, month, 1, 23, 59, 59, 999);
+    final end = DateTime(year, month + 1, 0, 23, 59, 59, 999);
     return (start: start, end: end);
   }
 
