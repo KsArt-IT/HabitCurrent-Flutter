dac63296cd0bea51146b3f23cb5e8e61684d3172
KsArT-IT <ksart.it@gmail.com>
2025-05-02 20:01:14 +0300

    feat(habit_month_card): add HabitMonthCard widget for displaying monthly habits

---
+++ app/lib/ui/habit_month/widgets/habit_month_card.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:habit_current/core/constants/constants.dart';
+import 'package:habit_current/l10n/intl_exp.dart';
+import 'package:habit_current/models/habit_month.dart';
+import 'package:habit_current/ui/habit_month/widgets/month_status_widget.dart';
+
+class HabitMonthCard extends StatelessWidget {
+  final HabitMonth habitMonth;
+
+  const HabitMonthCard({super.key, required this.habitMonth});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final strings = context.l10n;
+
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: Constants.paddingSmall),
+      padding: const EdgeInsets.fromLTRB(0, 0, 0, Constants.paddingAdjust),
+      decoration: BoxDecoration(
+        color: theme.colorScheme.surface,
+        borderRadius: BorderRadius.circular(Constants.borderRadius),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const SizedBox(width: Constants.paddingMedium),
+              Expanded(
+                child: Text(
+                  habitMonth.name,
+                  style: theme.textTheme.titleMedium,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              PopupMenuButton<String>(
+                padding: EdgeInsets.zero,
+                icon: Icon(
+                  Icons.more_horiz,
+                  color: theme.colorScheme.secondaryFixed,
+                  size: Constants.iconSize,
+                ),
+                itemBuilder:
+                    (context) => [
+                      PopupMenuItem(
+                        onTap: () {},
+                        child: Text(
+                          strings.edit,
+                          style: theme.textTheme.bodyMedium,
+                        ),
+                      ),
+                      PopupMenuItem(
+                        onTap: () {},
+                        child: Text(
+                          strings.delete,
+                          style: theme.textTheme.bodyMedium,
+                        ),
+                      ),
+                    ],
+              ),
+            ],
+          ),
+          // const SizedBox(height: Constants.paddingSmall),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: Constants.paddingMedium,
+            ),
+            child: MonthStatusWidget(weeksStatus: habitMonth.habitStatus),
+          ),
+        ],
+      ),
+    );
+  }
+}
