f30c6c7d5fb745dfd133cf42647f6c0ef4222fa4
KsArT-IT <ksart.it@gmail.com>
2025-05-02 22:30:30 +0300

    feat(month_name_widget): add MonthNameWidget for displaying formatted month names

---
+++ app/lib/ui/habit_month/widgets/month_name_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class MonthNameWidget extends StatelessWidget {
+  final DateTime month;
+  final bool selected;
+  final bool disabled;
+  const MonthNameWidget({
+    super.key,
+    required this.month,
+    this.selected = false,
+    this.disabled = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return disabled
+        ? const SizedBox(width: 50)
+        : SizedBox(
+          width: 50,
+          child: Text(
+            DateFormat('MMM').format(month),
+            style: theme.textTheme.titleMedium?.copyWith(
+              color:
+                  selected
+                      ? theme.colorScheme.onPrimary
+                      : theme.colorScheme.onTertiaryFixed,
+              fontWeight: selected ? FontWeight.w500 : FontWeight.w300,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        );
+  }
+}
