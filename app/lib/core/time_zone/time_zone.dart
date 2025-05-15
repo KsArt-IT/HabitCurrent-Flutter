12e59b957db4833b99f653f0eaa7fd9335def44f
KsArT-IT <ksart.it@gmail.com>
2025-05-15 19:55:08 +0300

    feat: implement TimeZone class for local time zone configuration

---
+++ app/lib/core/time_zone/time_zone.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_timezone/flutter_timezone.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
+abstract class TimeZone {
+  static Future<void> initialize() async {
+    await _configureLocalTimeZone();
+  }
+
+  // MARK: - Configure Local Time Zone
+  static Future<void> _configureLocalTimeZone() async {
+    if (kIsWeb || Platform.isLinux) {
+      return;
+    }
+    tz.initializeTimeZones();
+    if (Platform.isWindows) {
+      return;
+    }
+    final String? timeZoneName = await FlutterTimezone.getLocalTimezone();
+    tz.setLocalLocation(tz.getLocation(timeZoneName!));
+    // Initialize timezone
+    // if (Platform.isIOS || Platform.isMacOS) {
+    //   await FlutterTimezone.initializeTimeZone();
+    // }
+    if (kDebugMode) {
+      print('--------------------------------');
+      print('Local timezone: $timeZoneName');
+      print('--------------------------------');
+    }
+  }
+}
