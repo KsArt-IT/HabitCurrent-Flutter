bab71ae0b81d565e289609b52f775f029c679745
KsArT-IT <ksart.it@gmail.com>
2025-05-15 19:54:08 +0300

    feat: create HabitNotificationDatas table for managing habit notifications

---
+++ app/lib/data/database/table/habit_notification_table.dart
@@ -0,0 +1,14 @@
+import 'package:drift/drift.dart';
+
+class HabitNotificationDatas extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  IntColumn get userId => integer()();
+  IntColumn get habitId => integer()();
+  IntColumn get intervalId => integer()();
+
+  IntColumn get identifier => integer()();
+  TextColumn get title => text()();
+  IntColumn get weekDay => integer()();
+  IntColumn get time => integer()();
+  BoolColumn get repeats => boolean()();
+}
