668dbb3d0982c6018fdab29bb3121393102e1a8c
KsArT-IT <ksart.it@gmail.com>
2025-05-15 19:52:57 +0300

    feat: add HabitNotificationModel for managing habit notifications

---
+++ app/lib/data/models/habit_notification_model.dart
@@ -0,0 +1,49 @@
+final class HabitNotificationModel {
+  final int id;
+  final int userId;
+  final int habitId;
+  final int intervalId;
+
+  final int identifier;
+  final String title;
+  final int weekDay;
+  final int time;
+  final bool repeats;
+
+  HabitNotificationModel({
+    required this.id,
+    required this.userId,
+    required this.habitId,
+    required this.intervalId,
+    
+    required this.title,
+    required this.identifier,
+    required this.weekDay,
+    required this.time,
+    required this.repeats,
+  });
+
+  HabitNotificationModel copyWith({
+    int? id,
+    int? userId,
+    int? habitId,
+    int? intervalId,
+
+    int? identifier,
+    String? title,
+    int? weekDay,
+    int? time,
+    bool? repeats,
+  }) => HabitNotificationModel(
+    id: id ?? this.id,
+    userId: userId ?? this.userId,
+    habitId: habitId ?? this.habitId,
+    intervalId: intervalId ?? this.intervalId,
+
+    identifier: identifier ?? this.identifier,
+    title: title ?? this.title,
+    weekDay: weekDay ?? this.weekDay,
+    time: time ?? this.time,
+    repeats: repeats ?? this.repeats,
+  );
+}
