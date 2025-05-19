534a3ca15d8f81c7bd6f915638d7f27708b9df5a
KsArT-IT <ksart.it@gmail.com>
2025-05-19 18:35:27 +0300

    feat: implement LocalNotificationRepository

--- app/lib/data/repositories/notification/local_notification_repository.dart
+++ app/lib/data/repositories/notification/local_notification_repository.dart
@@ -4,6 +4,7 @@ import 'package:habit_current/data/repositories/notification/notification_reposi
 import 'package:habit_current/data/services/data/data_service.dart';
 import 'package:habit_current/data/services/notification/notification_service.dart';
 import 'package:habit_current/models/habit_notification.dart';
+import 'package:habit_current/models/weekdays.dart';
 import 'package:timezone/timezone.dart';
 
 final class LocalNotificationRepository implements NotificationRepository {
@@ -55,18 +56,49 @@ final class LocalNotificationRepository implements NotificationRepository {
   }
 
   @override
-  Future<void> scheduleNotifications(String title, List<HabitNotification> notifications) async {
+  Future<void> scheduleNotifications(
+    String title,
+    List<HabitNotification> notifications,
+  ) async {
     final date = DateTime.now();
     for (final notification in notifications) {
       final scheduledDate = notification.time.toDateTime(date);
       if (scheduledDate.isAfter(date)) {
         _notificationService.showNotificationOnDate(
-          id: notification.identifier,
+          id: notification.id,
           title: title,
           body: "",
           scheduledDate: TZDateTime.from(scheduledDate, local),
           payload: null,
         );
+      }
+    }
+  }
+
+  @override
+  Future<void> scheduleNotificationByHabitId(int habitId) async {
+    final notifications = await _service.loadNotificationsByHabitId(habitId);
+    print("LocalNotificationRepository: notifications: ${notifications.length}");
+
+    final date = DateTime.now();
+    for (final notification in notifications) {
+      final scheduledDate = notification.time.toDateTime(date);
+      // проверяем нужно ли сегодня показать
+      final isToday =
+          notification.weekDay == WeekDays.allDays ||
+          notification.weekDay == scheduledDate.weekday;
+      if (isToday && scheduledDate.isAfter(date)) {
+        _notificationService.showNotificationOnDate(
+          id: notification.id,
+          title: notification.title,
+          body: "",
+          scheduledDate: TZDateTime.from(scheduledDate, local),
+          payload:
+              "habit_${habitId}_time_${notification.intervalId}_day_${notification.weekDay}",
+        );
+        print("--------------------------------");
+        print("schedule notification: ${notification.title}: $isToday: $scheduledDate, now: $date");
+        print("--------------------------------");
       }
     }
   }
