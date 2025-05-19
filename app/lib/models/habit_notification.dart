e9faba22fbdf3e48fe91bfebc69df35af37f1b8c
KsArT-IT <ksart.it@gmail.com>
2025-05-19 18:20:54 +0300

    feat: change HabitNotification, change id and del identifier

--- app/lib/models/habit_notification.dart
+++ app/lib/models/habit_notification.dart
@@ -2,16 +2,14 @@ final class HabitNotification {
   final int id;
   final int intervalId;
 
-  final int identifier;
   final int weekDay;
   final int time;
   final bool repeats;
 
   HabitNotification({
-    required this.id,
+    this.id = 0,
     required this.intervalId,
     
-    required this.identifier,
     required this.weekDay,
     required this.time,
     required this.repeats,
