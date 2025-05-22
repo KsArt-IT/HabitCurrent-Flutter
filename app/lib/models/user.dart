931bd5b3bed3f9d4d6f9a6ff8dd1147e779a8dae
KsArT-IT <ksart.it@gmail.com>
2025-05-22 19:29:32 +0300

    refactor: add updated field to User model

--- app/lib/models/user.dart
+++ app/lib/models/user.dart
@@ -3,22 +3,26 @@ final class User {
   final String name;
   final String? avatar;
   final DateTime created;
+  final DateTime updated;
 
   User({
     required this.id,
     required this.name,
     this.avatar,
     required this.created,
+    required this.updated,
   });
 
   User copyWith({
     String? name,
     String? avatar,
-    DateTime? created, //
+    DateTime? created,
+    DateTime? updated,
   }) => User(
     id: id,
     name: name ?? this.name,
     avatar: avatar ?? this.avatar,
     created: created ?? this.created,
+    updated: updated ?? this.updated,
   );
 }
