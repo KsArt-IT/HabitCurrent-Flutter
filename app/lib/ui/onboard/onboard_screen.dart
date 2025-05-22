c13c5924a7df5bcf3e7b98b71c0944331eec9d3d
KsArT-IT <ksart.it@gmail.com>
2025-05-22 22:10:03 +0300

    refactor: replace AppBloc with InitialBloc in OnboardScreen

--- app/lib/ui/onboard/onboard_screen.dart
+++ app/lib/ui/onboard/onboard_screen.dart
@@ -1,9 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:habit_current/app/bloc/app_bloc.dart';
 import 'package:habit_current/core/theme/app_theme.dart';
 import 'package:habit_current/l10n/intl_exp.dart';
+import 'package:habit_current/ui/initial/bloc/initial_bloc.dart';
 import 'package:habit_current/ui/widgets/primary_button.dart';
 import 'package:habit_current/ui/widgets/radial_gradient_background.dart';
 
@@ -15,8 +15,6 @@ class OnboardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final strings = context.l10n;
-    final AppBloc appBloc = context.read<AppBloc>();
-    debugPrint('OnboardScreen build');
 
     return RadialGradientBackground(
       body: Column(
@@ -30,7 +28,7 @@ class OnboardScreen extends StatelessWidget {
                 PrimaryButton(
                   label: strings.continueBtn,
                   onPressed: () {
-                    appBloc.add(AppInitNameEvent());
+                    context.read<InitialBloc>().add(InitialHelloEvent());
                   },
                 ),
               ],
