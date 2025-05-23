6fb8ea449a31cc5799829290dc6f10d089dcf9df
KsArT-IT <ksart.it@gmail.com>
2025-05-23 19:55:41 +0300

    feat: add func in NotificationService

--- app/lib/data/services/notification/notification_service.dart
+++ app/lib/data/services/notification/notification_service.dart
@@ -4,6 +4,8 @@ import 'package:timezone/timezone.dart';
 
 abstract interface class NotificationService {
   Future<void> initialize();
+  Function(int habitId, int intervalId, int weekDay)? onNotificationReceived;
+  Function(int habitId)? onNotificationOpened;
 
   Future<Reminder> getNotificationPermissionStatus();
   Future<bool> checkNotificationPermission();
@@ -11,17 +13,32 @@ abstract interface class NotificationService {
   void openNotificationSettings();
 
   Future<void> showNotification({
-    required String title,
-    required String body,
-    String? payload,
-  });
-  Future<void> showNotificationOnDate({
     required int id,
     required String title,
     required String body,
     required TZDateTime scheduledDate,
     String? payload,
   });
+
+  Future<void> showNotificationOnDate({
+    required int id,
+    required String identifier,
+    required String title,
+    String? body,
+    required TZDateTime scheduledDate,
+  });
+
+  Future<void> scheduleNotificationOnWeekday({
+    required int id,
+    required String identifier,
+    required String title,
+    String? body,
+
+    required TZDateTime date, // current date and time
+    required int time, // minutes since midnight
+    required int weekday, // 1 = Monday, ..., 7 = Sunday // 127 = every day
+  });
+
   Future<void> cancelNotification(int id);
   Future<void> cancelAllNotifications();
 
