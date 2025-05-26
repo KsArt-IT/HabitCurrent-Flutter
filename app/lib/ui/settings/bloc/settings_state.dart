b25d5ce32cd333513f299e2f273f77f44b8ac7c3
KsArT-IT <ksart.it@gmail.com>
2025-05-26 18:46:36 +0300

    refactor: change SettingsState

--- app/lib/ui/settings/bloc/settings_state.dart
+++ app/lib/ui/settings/bloc/settings_state.dart
@@ -4,21 +4,30 @@ final class SettingsState extends Equatable {
   const SettingsState({
     this.name = '',
     this.language = 'en',
-    this.darkTheme = false,
+    this.themeName = 'system',
   });
 
   final String name;
   final String language;
-  final bool darkTheme;
+  final String themeName;
 
-  SettingsState copyWith({String? name, String? language, bool? darkTheme}) {
+  ThemeMode get theme => ThemeMode.values.firstWhere(
+    (e) => e.name == themeName,
+    orElse: () => ThemeMode.system,
+  );
+
+  SettingsState copyWith({
+    String? name,
+    String? language,
+    String? themeName, //
+  }) {
     return SettingsState(
       name: name ?? this.name,
       language: language ?? this.language,
-      darkTheme: darkTheme ?? this.darkTheme,
+      themeName: themeName ?? this.themeName,
     );
   }
 
   @override
-  List<Object?> get props => [name, language, darkTheme];
+  List<Object?> get props => [name, language, theme];
 }
