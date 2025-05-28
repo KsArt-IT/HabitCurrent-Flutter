cacf5222b6963424f58f37d0b5e123e7ec0dbdd5
KsArT-IT <ksart.it@gmail.com>
2025-05-28 20:23:08 +0300

    feat: add reminder field to SettingsState

--- app/lib/ui/settings/bloc/settings_state.dart
+++ app/lib/ui/settings/bloc/settings_state.dart
@@ -5,11 +5,13 @@ final class SettingsState extends Equatable {
     this.name = '',
     this.language = 'en',
     this.themeName = 'system',
+    this.reminder = true,
   });
 
   final String name;
   final String language;
   final String themeName;
+  final bool reminder;
 
   ThemeMode get theme => ThemeMode.values.firstWhere(
     (e) => e.name == themeName,
@@ -19,15 +21,23 @@ final class SettingsState extends Equatable {
   SettingsState copyWith({
     String? name,
     String? language,
-    String? themeName, //
+    String? themeName,
+    bool? reminder,
   }) {
     return SettingsState(
       name: name ?? this.name,
       language: language ?? this.language,
       themeName: themeName ?? this.themeName,
+      reminder: reminder ?? this.reminder,
     );
   }
 
   @override
-  List<Object?> get props => [name, language, theme];
+  List<Object?> get props => [
+    name,
+    language,
+    theme,
+    reminder,
+    //
+  ];
 }
