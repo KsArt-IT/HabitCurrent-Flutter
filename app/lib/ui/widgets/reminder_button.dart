5f8c2ad86a7b0e6dbf018b3f952a818a64122809
KsArT-IT <ksart.it@gmail.com>
2025-05-28 20:29:12 +0300

    refactor: improve handling of reminder state in ReminderButton

--- app/lib/ui/widgets/reminder_button.dart
+++ app/lib/ui/widgets/reminder_button.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:habit_current/app/bloc/app_bloc.dart';
+import 'package:habit_current/core/constants/constants.dart';
 import 'package:habit_current/models/reminder.dart';
+import 'package:habit_current/ui/settings/bloc/settings_bloc.dart';
 
 class ReminderButton extends StatefulWidget {
   const ReminderButton({super.key});
@@ -10,7 +12,8 @@ class ReminderButton extends StatefulWidget {
   State<ReminderButton> createState() => _ReminderButtonState();
 }
 
-class _ReminderButtonState extends State<ReminderButton> with WidgetsBindingObserver {
+class _ReminderButtonState extends State<ReminderButton>
+    with WidgetsBindingObserver {
   @override
   void initState() {
     super.initState();
@@ -33,34 +36,39 @@ class _ReminderButtonState extends State<ReminderButton> with WidgetsBindingObse
 
   @override
   Widget build(BuildContext context) {
-    final reminder = context.select((AppBloc bloc) => bloc.state.reminder);
-    debugPrint("reminder: $reminder");
-    
-    return Padding(
-      padding: const EdgeInsets.only(top: 32.0),
-      child: IconButton(
-        onPressed: () {
-          context.read<AppBloc>().add(
-            switch (reminder) {
-              // вкл/выкл
-              Reminder.enabled => AppReminderDisabledEvent(),
-              Reminder.disabled => AppReminderEnabledEvent(),
-              // запрос разрешения или перейти в настройки
-              Reminder.request => AppReminderRequestEvent(),
-              Reminder.open => AppReminderOpenEvent(),
+    return BlocBuilder<AppBloc, AppState>(
+      buildWhen: (previous, current) => previous.reminder != current.reminder,
+      builder: (context, state) {
+        final reminder =
+            state.reminder.isGranted
+                ? context.read<SettingsBloc>().state.reminder
+                    ? Reminder.enabled
+                    : Reminder.disabled
+                : state.reminder;
+        debugPrint("reminder: $reminder");
+
+        return Padding(
+          padding: const EdgeInsets.only(top: Constants.paddingXLarge),
+          child: IconButton(
+            onPressed: () {
+              context.read<AppBloc>().add(AppReminderChangeEvent(reminder));
+              context.read<SettingsBloc>().add(
+                // если уведомления включены, то выключить, иначе включить.
+                SettingsUpdateReminderEvent(reminder != Reminder.enabled),
+              );
             },
-          );
-        },
-        icon: switch (reminder) {
-          Reminder.enabled => const Icon(Icons.notifications_active),
-          Reminder.disabled => const Icon(Icons.notifications_off),
-          Reminder.request => const Icon(Icons.notification_important),
-          Reminder.open => const Icon(Icons.edit_notifications),
-        },
-        padding: const EdgeInsets.all(8),
-        iconSize: 24,
-        color: Theme.of(context).colorScheme.onPrimary,
-      ),
+            icon: switch (reminder) {
+              Reminder.enabled => const Icon(Icons.notifications_active),
+              Reminder.disabled => const Icon(Icons.notifications_off),
+              Reminder.request => const Icon(Icons.notification_important),
+              Reminder.open => const Icon(Icons.edit_notifications),
+            },
+            padding: const EdgeInsets.all(Constants.paddingSmall),
+            iconSize: Constants.iconSize,
+            color: Theme.of(context).colorScheme.onPrimary,
+          ),
+        );
+      },
     );
   }
 }
