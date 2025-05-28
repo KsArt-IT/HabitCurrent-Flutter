1df907ad23162f2cc7eebe5a1cb8e244ee7a8e3b
KsArT-IT <ksart.it@gmail.com>
2025-05-28 20:27:04 +0300

    feat: add reminder change event handler to SettingsBloc

--- app/lib/ui/settings/bloc/settings_bloc.dart
+++ app/lib/ui/settings/bloc/settings_bloc.dart
@@ -16,6 +16,7 @@ class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
     on<SettingsUpdateNameEvent>(_onNameChanged);
     on<SettingsUpdateLanguageEvent>(_onLanguageChanged);
     on<SettingsUpdateThemeEvent>(_onThemeChanged);
+    on<SettingsUpdateReminderEvent>(_onReminderChanged);
     on<SettingsSaveEvent>(_onSave);
     on<SettingsResetEvent>(_onReset);
   }
@@ -27,7 +28,15 @@ class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
     final language = await _settingsRepository.loadLanguage();
     final name = await _settingsRepository.loadName();
     final themeName = await _settingsRepository.loadThemeMode();
-    emit(state.copyWith(language: language, name: name, themeName: themeName));
+    final reminder = await _settingsRepository.loadReminder();
+    emit(
+      state.copyWith(
+        language: language,
+        name: name,
+        themeName: themeName,
+        reminder: reminder,
+      ),
+    );
   }
 
   void _onNameChanged(
@@ -54,10 +63,19 @@ class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
     emit(state.copyWith(themeName: event.themeName));
   }
 
+  void _onReminderChanged(
+    SettingsUpdateReminderEvent event,
+    Emitter<SettingsState> emit,
+  ) async {
+    await _settingsRepository.saveReminder(event.reminder);
+    emit(state.copyWith(reminder: event.reminder));
+  }
+
   void _onSave(SettingsSaveEvent event, Emitter<SettingsState> emit) async {
     await _settingsRepository.saveName(state.name);
     await _settingsRepository.saveLanguage(state.language);
     await _settingsRepository.saveThemeMode(state.themeName);
+    await _settingsRepository.saveReminder(state.reminder);
   }
 
   void _onReset(SettingsResetEvent event, Emitter<SettingsState> emit) async {
@@ -65,6 +83,7 @@ class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
     await _settingsRepository.saveName(newState.name);
     await _settingsRepository.saveLanguage(newState.language);
     await _settingsRepository.saveThemeMode(newState.themeName);
+    await _settingsRepository.saveReminder(newState.reminder);
     emit(newState);
   }
 }
