4f83e990884cd06d2953a5ec21c0a8ebd93530f6
KsArT-IT <ksart.it@gmail.com>
2025-05-30 17:44:36 +0300

    feat: implement react to notification in appBloc

--- app/lib/app/bloc/app_bloc.dart
+++ app/lib/app/bloc/app_bloc.dart
@@ -4,6 +4,7 @@ import 'package:habit_current/data/repositories/data/data_repository.dart';
 import 'package:habit_current/data/repositories/notification/notification_repository.dart';
 import 'package:habit_current/models/habit.dart';
 import 'package:habit_current/models/hour_interval_completed.dart';
+import 'package:habit_current/models/notification_response_details.dart';
 import 'package:habit_current/models/reminder.dart';
 import 'package:habit_current/models/user.dart';
 
@@ -35,54 +36,21 @@ final class AppBloc extends Bloc<AppEvent, AppState> {
     notificationRepository.observeNotificationReceived(_onNotificationReceived);
   }
 
-  // MARK: - Notification Received
-  Future<void> _onNotificationReceived(String identifier, bool isOpen) async {
-    final params = identifier.split('_');
-    if (params.length < 8) return;
-
-    final userId = int.parse(params[1]);
-    final habitId = int.parse(params[3]);
-    final intervalId = int.parse(params[5]);
-    final weekDay = int.parse(params[7]);
-
-    print('--------------------------------');
-    print('identifier: $identifier');
-    print('isOpen: $isOpen');
-    print('userId: $userId login: ${state.user?.id}');
-    print('habitId: $habitId');
-    print('intervalId: $intervalId');
-    print('weekDay: $weekDay');
-    print('--------------------------------');
-    if (userId != state.user?.id) return;
-    if (isOpen) {
-      final habit = await dataRepository.loadHabitById(habitId, DateTime.now());
-      if (habit != null) {
-        add(AppHabitViewEvent(habit: habit));
-      }
-    } else {
-      print('AppHabitMakeDoneEvent');
-      add(
-        AppHabitMakeDoneEvent(
-          habitId: habitId,
-          intervalId: intervalId,
-          weekDay: weekDay,
-        ),
-      );
-    }
-    print('--------------------------------');
-  }
-
   void _onInitialEvent(AppInitialEvent event, Emitter<AppState> emit) async {
     emit(AppState.initial());
   }
 
+  // MARK: - User Loaded
   void _onUserLoadedEvent(
     AppUserLoadedEvent event,
     Emitter<AppState> emit,
   ) async {
     emit(state.copyWith(status: AppStatus.userLoaded, user: event.user));
+    // Проверяем, было ли приложение запущено через уведомление
+    _onNotificationReceivedAppLaunch();
   }
 
+  // MARK: - Habit Events
   void _onHabitCreateEvent(
     AppHabitCreateEvent event,
     Emitter<AppState> emit,
@@ -172,7 +140,7 @@ final class AppBloc extends Bloc<AppEvent, AppState> {
     );
   }
 
-  // MARK: - Reminder
+  // MARK: - Reminder Events
   void _onReminderCheckEvent(
     AppReminderCheckEvent event,
     Emitter<AppState> emit,
@@ -203,7 +171,8 @@ final class AppBloc extends Bloc<AppEvent, AppState> {
         _onReminderOpenSettings();
         return;
       case Reminder.request:
-        final permission = await notificationRepository.requestNotificationPermission();
+        final permission =
+            await notificationRepository.requestNotificationPermission();
         reminder = permission ? Reminder.enabled : Reminder.open;
       case Reminder.enabled:
         // отменить все уведомления
@@ -229,6 +198,67 @@ final class AppBloc extends Bloc<AppEvent, AppState> {
 
   void _onReminderOpenSettings() {
     notificationRepository.openNotificationSettings();
+  }
+
+  // MARK: - Notification Received
+  Future<void> _onNotificationReceivedAppLaunch() async {
+    final notification =
+        await notificationRepository.getNotificationAppLaunchDetails();
+    if (notification != null) {
+      await _onNotificationReceived(notification);
+    }
+  }
+
+  Future<void> _onNotificationReceived(
+    NotificationResponseDetails notification,
+  ) async {
+    if (notification.laterMinutes != null) {
+      // schedule notification with later minutes
+      await notificationRepository.showNotificationLater(
+        id: notification.id,
+        identifier: notification.identifier,
+        laterMinutes: notification.laterMinutes!,
+      );
+    } else {
+      await _reactToNotification(notification.identifier, notification.isOpen);
+    }
+  }
+
+  Future<void> _reactToNotification(String identifier, bool isOpen) async {
+    final params = identifier.split('_');
+    if (params.length < 8) return;
+
+    final userId = int.parse(params[1]);
+    final habitId = int.parse(params[3]);
+    final intervalId = int.parse(params[5]);
+    final weekDay = int.parse(params[7]);
+
+    print('--------------------------------');
+    print('identifier: $identifier');
+    print('isOpen: $isOpen');
+    print('userId: $userId login: ${state.user?.id}');
+    print('habitId: $habitId');
+    print('intervalId: $intervalId');
+    print('weekDay: $weekDay');
+    print('--------------------------------');
+    if (userId != state.user?.id) return;
+    if (isOpen) {
+      final habit = await dataRepository.loadHabitById(habitId, DateTime.now());
+      if (habit != null) {
+        add(AppHabitViewEvent(habit: habit));
+      }
+    } else {
+      print('AppHabitMakeDoneEvent');
+      add(
+        AppHabitMakeDoneEvent(
+          habitId: habitId,
+          intervalId: intervalId,
+          weekDay: weekDay,
+        ),
+      );
+      add(AppHabitReloadEvent(habitId: habitId));
+    }
+    print('--------------------------------');
   }
 
   // MARK: - Show Test Notification
