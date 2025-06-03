f185496a020084d2830641087a8cf1731a678f43
KsArT-IT <ksart.it@gmail.com>
2025-06-03 16:58:15 +0300

    feat(habit_view): add error handling to HabitViewState

--- app/lib/ui/habit_view/bloc/habit_view_state.dart
+++ app/lib/ui/habit_view/bloc/habit_view_state.dart
@@ -2,13 +2,27 @@ part of 'habit_view_bloc.dart';
 
 class HabitViewState extends Equatable {
   final Habit habit;
+  final HabitStateStatus status;
+  final AppError? error;
 
-  const HabitViewState({required this.habit});
+  const HabitViewState({
+    required this.habit,
+    this.status = HabitStateStatus.initial,
+    this.error,
+  });
 
-  HabitViewState copyWith({Habit? habit}) {
-    return HabitViewState(habit: habit ?? this.habit);
+  HabitViewState copyWith({
+    Habit? habit,
+    HabitStateStatus? status,
+    AppError? error,
+  }) {
+    return HabitViewState(
+      habit: habit ?? this.habit,
+      status: status ?? this.status,
+      error: error ?? this.error,
+    );
   }
 
   @override
-  List<Object?> get props => [habit];
+  List<Object?> get props => [habit, status, error];
 }
