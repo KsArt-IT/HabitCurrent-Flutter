e2aee4b25ec81918a0781d78dcc0a042e97356c6
KsArT-IT <ksart.it@gmail.com>
2025-06-03 17:22:36 +0300

    feat(habit_view_bloc): improve management of interval execution and notifications

--- app/lib/ui/habit_view/bloc/habit_view_bloc.dart
+++ app/lib/ui/habit_view/bloc/habit_view_bloc.dart
@@ -1,17 +1,27 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:habit_current/core/error/app_error.dart';
 import 'package:habit_current/data/repositories/data/data_repository.dart';
+import 'package:habit_current/data/repositories/notification/notification_repository.dart';
 import 'package:habit_current/models/habit.dart';
+import 'package:habit_current/models/habit_state_status.dart';
+import 'package:habit_current/models/hour_interval.dart';
 import 'package:habit_current/models/hour_interval_completed.dart';
 
 part 'habit_view_event.dart';
 part 'habit_view_state.dart';
 
 class HabitViewBloc extends Bloc<HabitViewEvent, HabitViewState> {
-  final DataRepository dataRepository;
+  final DataRepository _dataRepository;
+  final NotificationRepository _notificationRepository;
 
-  HabitViewBloc({required this.dataRepository, required Habit habit})
-    : super(HabitViewState(habit: habit)) {
+  HabitViewBloc({
+    required DataRepository dataRepository,
+    required NotificationRepository notificationRepository,
+    required Habit habit,
+  }) : _dataRepository = dataRepository,
+       _notificationRepository = notificationRepository,
+       super(HabitViewState(habit: habit)) {
     on<HabitViewChangedEvent>(_onHabitViewChangedEvent);
   }
 
@@ -22,35 +32,85 @@ class HabitViewBloc extends Bloc<HabitViewEvent, HabitViewState> {
     print('--------------------------------');
     print('event.index: ${event.index}');
     print('--------------------------------');
-    if (event.index >= 0 && event.index < state.habit.intervals.length) {
+    if (event.index < 0 || event.index >= state.habit.intervals.length) return;
+    try {
       final interval = state.habit.intervals[event.index];
       print('interval: ${interval.id}');
-      final completedIntervals = state.habit.completedIntervals.toList();
-      HourIntervalCompleted? completed = completedIntervals
-          .where((e) => e.intervalId == interval.id)
-          .firstOrNull;
-      if (completed == null) {
-        completed = HourIntervalCompleted(
-          intervalId: interval.id,
-          time: interval.time,
-          completed: DateTime.now(),
-        );
-        completed = await dataRepository.createHourIntervalCompleted(
+      // изменим статус выполнения интервала и получим список выполненных интервалов
+      final (completedIntervals, todayCompleted) = await _changeAndGetCompleted(
+        interval,
+      );
+      // изменим уведомление на завтра или сегодня
+      await _changeNotification(
+        intervalId: interval.id,
+        tomorrow: todayCompleted,
+      );
+      // обновим список выполненных интервалов
+      emit(
+        state.copyWith(
+          status: HabitStateStatus.initial,
+          habit: state.habit.copyWith(completedIntervals: completedIntervals),
+        ),
+      );
+    } catch (e) {
+      emit(
+        state.copyWith(
+          status: HabitStateStatus.error,
+          error: e is AppError ? e : UnknownError(e.toString()),
+        ),
+      );
+    }
+  }
+
+  Future<(List<HourIntervalCompleted>, bool)> _changeAndGetCompleted(
+    HourInterval interval,
+  ) async {
+    bool todayCompleted = true;
+    final completedIntervals = state.habit.completedIntervals.toList();
+    HourIntervalCompleted? completed =
+        completedIntervals
+            .where((e) => e.intervalId == interval.id)
+            .firstOrNull;
+    if (completed == null) {
+      completed = HourIntervalCompleted(
+        intervalId: interval.id,
+        time: interval.time,
+        completed: DateTime.now(),
+      );
+      try {
+        completed = await _dataRepository.createHourIntervalCompleted(
           state.habit.id,
           completed,
         );
         completedIntervals.add(completed);
-        print('completed: ${completed.id} ${completed.intervalId}');
-      } else {
-        dataRepository.deleteHourIntervalCompletedById(completed.id);
-        completedIntervals.remove(completed);
-        print('remove: ${completed.id} ${completed.intervalId}');
+      } catch (e) {
+        throw DatabaseCreatingError(e.toString());
       }
-      emit(
-        state.copyWith(
-          habit: state.habit.copyWith(completedIntervals: completedIntervals),
-        ),
+      print('completed: ${completed.id} ${completed.intervalId}');
+    } else {
+      try {
+        await _dataRepository.deleteHourIntervalCompletedById(completed.id);
+      } catch (e) {
+        throw DatabaseDeletingError(e.toString());
+      }
+      completedIntervals.remove(completed);
+      todayCompleted = false;
+      print('remove: ${completed.id} ${completed.intervalId}');
+    }
+    return (completedIntervals, todayCompleted);
+  }
+
+  Future<void> _changeNotification({
+    required int intervalId,
+    required bool tomorrow,
+  }) async {
+    try {
+      await _notificationRepository.scheduleNotificationByIntervalId(
+        intervalId,
+        tomorrow,
       );
+    } catch (e) {
+      throw NotificationError(e.toString());
     }
   }
 }
