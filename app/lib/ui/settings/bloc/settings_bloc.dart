4058890a0bba92e465696cc8f0c02119c3ddea0b
KsArT-IT <ksart.it@gmail.com>
2025-06-04 16:40:08 +0300

    feat(settings_bloc): add error handling when loading and saving settings

--- app/lib/ui/settings/bloc/settings_bloc.dart
+++ app/lib/ui/settings/bloc/settings_bloc.dart
@@ -1,7 +1,9 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:habit_current/core/error/app_error.dart';
 import 'package:habit_current/data/repositories/settings/settings_repository.dart';
+import 'package:habit_current/models/state_status.dart';
 
 part 'settings_event.dart';
 part 'settings_state.dart';
@@ -25,65 +27,116 @@ class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
     SettingsLoadEvent event,
     Emitter<SettingsState> emit,
   ) async {
-    final language = await _settingsRepository.loadLanguage();
-    final name = await _settingsRepository.loadName();
-    final themeName = await _settingsRepository.loadThemeMode();
-    final reminder = await _settingsRepository.loadReminder();
-    emit(
-      state.copyWith(
-        language: language,
-        name: name,
-        themeName: themeName,
-        reminder: reminder,
-      ),
-    );
+    try {
+      final language = await _settingsRepository.loadLanguage();
+      final name = await _settingsRepository.loadName();
+      final themeName = await _settingsRepository.loadThemeMode();
+      final reminder = await _settingsRepository.loadReminder();
+      emit(
+        state.copyWith(
+          language: language,
+          name: name,
+          themeName: themeName,
+          reminder: reminder,
+        ),
+      );
+    } catch (e) {
+      emit(
+        state.copyWith(
+          status: StateStatus.error,
+          error: SettingsLoadingError(e.toString()),
+        ),
+      );
+    }
   }
 
   void _onNameChanged(
     SettingsUpdateNameEvent event,
     Emitter<SettingsState> emit,
   ) async {
-    await _settingsRepository.saveName(event.name);
-    emit(state.copyWith(name: event.name));
+    try {
+      await _settingsRepository.saveName(event.name);
+      emit(state.copyWith(name: event.name));
+    } catch (e) {
+      emit(
+        state.copyWith(
+          status: StateStatus.error,
+          error: SettingsSavingError(e.toString()),
+        ),
+      );
+    }
   }
 
   void _onLanguageChanged(
     SettingsUpdateLanguageEvent event,
     Emitter<SettingsState> emit,
   ) async {
-    await _settingsRepository.saveLanguage(event.language);
-    emit(state.copyWith(language: event.language));
+    try {
+      await _settingsRepository.saveLanguage(event.language);
+      emit(state.copyWith(language: event.language));
+    } catch (e) {
+      emit(
+        state.copyWith(
+          status: StateStatus.error,
+          error: SettingsSavingError(e.toString()),
+        ),
+      );
+    }
   }
 
   void _onThemeChanged(
     SettingsUpdateThemeEvent event,
     Emitter<SettingsState> emit,
   ) async {
-    await _settingsRepository.saveThemeMode(event.themeName);
-    emit(state.copyWith(themeName: event.themeName));
+    try {
+      await _settingsRepository.saveThemeMode(event.themeName);
+      emit(state.copyWith(themeName: event.themeName));
+    } catch (e) {
+      emit(
+        state.copyWith(
+          status: StateStatus.error,
+          error: SettingsSavingError(e.toString()),
+        ),
+      );
+    }
   }
 
   void _onReminderChanged(
     SettingsUpdateReminderEvent event,
     Emitter<SettingsState> emit,
   ) async {
-    await _settingsRepository.saveReminder(event.reminder);
-    emit(state.copyWith(reminder: event.reminder));
+    try {
+      await _settingsRepository.saveReminder(event.reminder);
+      emit(state.copyWith(reminder: event.reminder));
+    } catch (e) {
+      emit(
+        state.copyWith(
+          status: StateStatus.error,
+          error: SettingsSavingError(e.toString()),
+        ),
+      );
+    }
   }
 
   void _onSave(SettingsSaveEvent event, Emitter<SettingsState> emit) async {
-    await _settingsRepository.saveName(state.name);
-    await _settingsRepository.saveLanguage(state.language);
-    await _settingsRepository.saveThemeMode(state.themeName);
-    await _settingsRepository.saveReminder(state.reminder);
+    try {
+      await _settingsRepository.saveName(state.name);
+      await _settingsRepository.saveLanguage(state.language);
+      await _settingsRepository.saveThemeMode(state.themeName);
+      await _settingsRepository.saveReminder(state.reminder);
+    } catch (e) {
+      emit(
+        state.copyWith(
+          status: StateStatus.error,
+          error: SettingsSavingError(e.toString()),
+        ),
+      );
+    }
   }
 
   void _onReset(SettingsResetEvent event, Emitter<SettingsState> emit) async {
     final newState = SettingsState();
-    await _settingsRepository.saveName(newState.name);
-    await _settingsRepository.saveLanguage(newState.language);
-    await _settingsRepository.saveThemeMode(newState.themeName);
-    await _settingsRepository.saveReminder(newState.reminder);
+    add(SettingsSaveEvent());
     emit(newState);
   }
 }
