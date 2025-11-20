1725e3c60e99bce12a85edf5ec117e13b781aaef
KsArT-IT <ksart.it@gmail.com>
2025-11-20 21:46:17 +0200

    style(time_zone): remove nullable type annotation from timeZoneName

--- app/lib/core/time_zone/time_zone.dart
+++ app/lib/core/time_zone/time_zone.dart
@@ -19,11 +19,11 @@ abstract class TimeZone {
     if (Platform.isWindows) {
       return;
     }
-    final TimezoneInfo? timeZoneName = await FlutterTimezone.getLocalTimezone();
-    tz.setLocalLocation(tz.getLocation(timeZoneName?.identifier ?? ''));
+    final TimezoneInfo timeZoneName = await FlutterTimezone.getLocalTimezone();
+    tz.setLocalLocation(tz.getLocation(timeZoneName.identifier));
     if (kDebugMode) {
       print('--------------------------------');
-      print('Local timezone: ${timeZoneName?.identifier}');
+      print('Local timezone: ${timeZoneName.identifier}');
       print('--------------------------------');
     }
   }
